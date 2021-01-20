0a0384de9e97639eb0ae38a60efff4487abac112
ioangut <ioan.gut@sap.com>
2021-01-20 15:17:48 +0200

    add local postgres db configs

--- services/edus/src/main/java/app/coronawarn/analytics/services/edus/otp/OtpResponse.java
+++ services/edus/src/main/java/app/coronawarn/analytics/services/edus/otp/OtpResponse.java
@@ -2,4 +2,22 @@ package app.coronawarn.analytics.services.edus.otp;
 
 public class OtpResponse {
 
+    private String otp;
+    private Boolean valid;
+
+    public Boolean getValid() {
+        return valid;
+    }
+
+    public void setValid(Boolean valid) {
+        this.valid = valid;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
 }
