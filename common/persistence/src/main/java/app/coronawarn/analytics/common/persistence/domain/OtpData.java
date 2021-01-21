f362516a134e8e9ccbe21012d38bd8a57fa36062
ioangut <ioan.gut@sap.com>
2021-01-21 12:03:49 +0200

    Update OtpData.java

--- common/persistence/src/main/java/app/coronawarn/analytics/common/persistence/domain/OtpData.java
+++ common/persistence/src/main/java/app/coronawarn/analytics/common/persistence/domain/OtpData.java
@@ -1,34 +1,34 @@
 package app.coronawarn.analytics.common.persistence.domain;
 
+import java.util.Date;
+import javax.validation.constraints.Size;
 import org.springframework.data.annotation.Id;
 
-import javax.validation.constraints.Size;
-import java.util.Date;
-
 public class OtpData {
-    @Id
-    @Size(min = 36, max = 36)
-    private String otp;
-    private Date expirationDate;
 
-    public OtpData(String otp, Date expirationDate) {
-        this.otp = otp;
-        this.expirationDate = expirationDate;
-    }
+  @Id
+  @Size(min = 36, max = 36)
+  private String otp;
+  private Date expirationDate;
 
-    public String getOtp() {
-        return otp;
-    }
+  public OtpData(String otp, Date expirationDate) {
+    this.otp = otp;
+    this.expirationDate = expirationDate;
+  }
 
-    public void setOtp(String otp) {
-        this.otp = otp;
-    }
+  public String getOtp() {
+    return otp;
+  }
 
-    public Date getExpirationDate() {
-        return expirationDate;
-    }
+  public void setOtp(String otp) {
+    this.otp = otp;
+  }
 
-    public void setExpirationDate(Date expirationDate) {
-        this.expirationDate = expirationDate;
-    }
+  public Date getExpirationDate() {
+    return expirationDate;
+  }
+
+  public void setExpirationDate(Date expirationDate) {
+    this.expirationDate = expirationDate;
+  }
 }
