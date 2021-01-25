cc2b2cbc7265ff6e7a8fc3f5ab9b68b30a06c6f0
ioangut <67064882+ioangut@users.noreply.github.com>
2021-01-25 16:00:19 +0200

    Update services/edus/src/main/java/app/coronawarn/datadonation/services/edus/otp/OtpRequest.java
    Co-authored-by: Hilmar Falkenberg <hilmar.falkenberg@sap.com>

--- services/edus/src/main/java/app/coronawarn/datadonation/services/edus/otp/OtpRequest.java
+++ services/edus/src/main/java/app/coronawarn/datadonation/services/edus/otp/OtpRequest.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.Size;
 
 public class OtpRequest {
 
+  /** UUID */
   @Pattern(regexp = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[34][0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}")
   private String otp;
 
