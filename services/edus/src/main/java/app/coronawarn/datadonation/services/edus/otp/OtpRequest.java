7615dddad250d4e90bf938735120fd2cdb3b0a56
ioangut <ioan.gut@sap.com>
2021-01-25 13:11:54 +0200

    add OtpRequest UUID pattern validation

--- services/edus/src/main/java/app/coronawarn/datadonation/services/edus/otp/OtpRequest.java
+++ services/edus/src/main/java/app/coronawarn/datadonation/services/edus/otp/OtpRequest.java
@@ -1,10 +1,11 @@
 package app.coronawarn.datadonation.services.edus.otp;
 
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class OtpRequest {
 
-  @Size(min = 36, max = 36)
+  @Pattern(regexp = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[34][0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}")
   private String otp;
 
   public String getOtp() {
