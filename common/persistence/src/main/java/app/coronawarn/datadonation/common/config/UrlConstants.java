277845f63b99c9b1c901c5026243c0e3983919c6
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-01-29 16:29:05 +0100

    Common/config (#31)
    * API paths
    
    * slf4j SECURITY marker for relevant log messages
    
    * ignore /temp/ folder
    
    * AndroidController using UrlConstants
    
    * IosController using UrlConstants
    
    * OtpController using UrlConstants

---
+++ common/persistence/src/main/java/app/coronawarn/datadonation/common/config/UrlConstants.java
@@ -0,0 +1,44 @@
+package app.coronawarn.datadonation.common.config;
+
+public interface UrlConstants {
+
+  /**
+   * {@value}.
+   */
+  static final String BASE = "/version";
+
+  /**
+   * {@value}.
+   */
+  static final String V1 = BASE + "/v1";
+
+  /**
+   * {@value}.
+   */
+  static final String SURVEY = BASE + "/v1";
+
+  /**
+   * {@value}.
+   */
+  static final String DATA = "/dat";
+
+  /**
+   * {@value}.
+   */
+  static final String OTP = "/otp";
+
+  /**
+   * {@value}.
+   */
+  static final String LOG = "/log";
+
+  /**
+   * {@value}.
+   */
+  static final String ANDROID = V1 + "/android";
+
+  /**
+   * {@value}.
+   */
+  static final String IOS = V1 + "/ios";
+}
