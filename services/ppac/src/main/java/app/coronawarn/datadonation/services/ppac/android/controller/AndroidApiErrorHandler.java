e45c0835ff9834fdb03a876575a44b05ccd75ef9
EugenM-SAP <67458405+EugenM-SAP@users.noreply.github.com>
2021-02-01 15:42:44 +0200

    Add error codes to the API (#33)

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/android/controller/AndroidApiErrorHandler.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/android/controller/AndroidApiErrorHandler.java
@@ -9,6 +9,8 @@ import app.coronawarn.datadonation.services.ppac.android.attestation.errors.Fail
 import app.coronawarn.datadonation.services.ppac.android.attestation.errors.MissingMandatoryAuthenticationFields;
 import app.coronawarn.datadonation.services.ppac.android.attestation.errors.NonceCalculationError;
 import app.coronawarn.datadonation.services.ppac.android.attestation.errors.NonceCouldNotBeVerified;
+import app.coronawarn.datadonation.services.ppac.android.attestation.errors.SaltNotValidAnymore;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -24,39 +26,54 @@ public class AndroidApiErrorHandler extends ResponseEntityExceptionHandler {
 
   private static final Logger logger = LoggerFactory.getLogger(AndroidApiErrorHandler.class);
 
+  /**
+   * Mapping of business logic exceptions to codes delivered to the client.
+   */
+  private static final Map<Class<? extends RuntimeException>, ApiErrorCode> ERROR_STATES =
+      Map.of(FailedJwsParsing.class, ApiErrorCode.JWS_SIGNATURE_VERIFICATION_FAILED,
+          FailedSignatureVerification.class, ApiErrorCode.JWS_SIGNATURE_VERIFICATION_FAILED,
+          SaltNotValidAnymore.class, ApiErrorCode.JWS_SIGNATURE_VERIFICATION_FAILED,
+          FailedAttestationTimestampValidation.class, ApiErrorCode.ATTESTATION_EXPIRED,
+          NonceCouldNotBeVerified.class, ApiErrorCode.NONCE_MISMATCH,
+          ApkPackageNameNotAllowed.class, ApiErrorCode.APK_PACKAGE_NAME_MISMATCH,
+          ApkCertificateDigestsNotAllowed.class, ApiErrorCode.APK_CERTIFICATE_MISMATCH);
+
   @ExceptionHandler(value = {FailedJwsParsing.class, FailedSignatureVerification.class})
   protected ResponseEntity<Object> handleAuthenticationErrors(RuntimeException runtimeException,
       WebRequest webRequest) {
     logger.warn(runtimeException.getMessage());
-    return handleExceptionInternal(runtimeException, null, new HttpHeaders(),
-        HttpStatus.UNAUTHORIZED, webRequest);
-
+    return handleExceptionInternal(runtimeException, getErrorCode(runtimeException),
+        new HttpHeaders(), HttpStatus.UNAUTHORIZED, webRequest);
   }
 
   @ExceptionHandler(value = {FailedAttestationTimestampValidation.class,
       FailedAttestationHostnameValidation.class, ApkPackageNameNotAllowed.class,
-      ApkCertificateDigestsNotAllowed.class, NonceCouldNotBeVerified.class})
+      ApkCertificateDigestsNotAllowed.class, NonceCouldNotBeVerified.class,
+      SaltNotValidAnymore.class})
   protected ResponseEntity<Object> handleForbiddenErrors(RuntimeException runtimeException,
       WebRequest webRequest) {
     logger.warn(runtimeException.getMessage());
-    return handleExceptionInternal(runtimeException, null, new HttpHeaders(), HttpStatus.FORBIDDEN,
-        webRequest);
+    return handleExceptionInternal(runtimeException, getErrorCode(runtimeException),
+        new HttpHeaders(), HttpStatus.FORBIDDEN, webRequest);
   }
 
   @ExceptionHandler(value = MissingMandatoryAuthenticationFields.class)
   protected ResponseEntity<Object> handleMissingInformationOrBadRequests(
       RuntimeException runtimeException, WebRequest webRequest) {
     logger.warn(runtimeException.getMessage());
-    return handleExceptionInternal(runtimeException, null, new HttpHeaders(),
-        HttpStatus.BAD_REQUEST, webRequest);
+    return handleExceptionInternal(runtimeException, runtimeException.getMessage(),
+        new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
   }
 
   @ExceptionHandler(value = NonceCalculationError.class)
   protected ResponseEntity<Object> handleInternalServerErrors(RuntimeException runtimeException,
       WebRequest webRequest) {
     logger.error(runtimeException.getMessage());
-    return new ResponseEntity<>(
-        "Server encountered an internal issue while validating nonce from the attestation",
-        new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    return new ResponseEntity<>(ApiErrorCode.INTERNAL_SERVER_ERROR, new HttpHeaders(),
+        HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  private Object getErrorCode(RuntimeException runtimeException) {
+    return ERROR_STATES.getOrDefault(runtimeException.getClass(), ApiErrorCode.UNKNOWN);
   }
 }
