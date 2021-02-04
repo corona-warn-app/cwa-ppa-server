94aa0c1013fb0fbe8d82156ba1754eb92b8a71a7
FelixRottler <fero429@gmail.com>
2021-02-04 17:29:23 +0100

    feature: use correct schema for database migrations

--- common/persistence/src/main/resources/db/migration/V1__createTables.sql
+++ common/persistence/src/main/resources/db/migration/V1__createTables.sql
@@ -1,34 +1,34 @@
 -- see metrics.proto
 
-CREATE TABLE int_data (
+CREATE TABLE data_donation.int_data (
     os VARCHAR(10) NOT NULL,
     metric INTEGER NOT NULL,
     m_val BIGINT NOT NULL,
     m_day DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE text_data (
+CREATE TABLE data_donation.text_data (
     os VARCHAR(10) NOT NULL,
     metric INTEGER NOT NULL,
     m_val TEXT NOT NULL,
     m_day DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE float_data (
+CREATE TABLE data_donation.float_data (
     os VARCHAR(10) NOT NULL,
     metric INTEGER NOT NULL,
     m_val DOUBLE PRECISION NOT NULL,
     m_day DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE one_time_password (
+CREATE TABLE data_donation.one_time_password (
     password VARCHAR(36) PRIMARY KEY,
     creation_timestamp BIGINT NOT NULL,
     redemption_timestamp BIGINT,
     last_validity_check_timestamp BIGINT
 );
 
-CREATE TABLE api_token (
+CREATE TABLE data_donation.api_token (
     api_token VARCHAR PRIMARY KEY,
     expiration_date BIGINT NOT NULL,
     created_at BIGINT NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE api_token (
     last_used_ppac BIGINT
 );
 
-CREATE TABLE device_token (
+CREATE TABLE data_donation.device_token (
   id SERIAL PRIMARY KEY,
   device_token_hash  BYTEA UNIQUE NOT NULL,
   created_at BIGINT NOT NULL
