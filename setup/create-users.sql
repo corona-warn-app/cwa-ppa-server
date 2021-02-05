4e879f0519677ea2ee87fcc979db67bfa5696728
FelixRottler <fero429@gmail.com>
2021-02-05 16:15:16 +0100

    feature: add create user template

---
+++ setup/create-users.sql
@@ -0,0 +1,3 @@
+CREATE USER "<change me>" WITH INHERIT IN ROLE cwa_ppdd_flyway PASSWORD '<change me>';
+CREATE USER "<change me>" WITH INHERIT IN ROLE cwa_ppdd_ppac PASSWORD "<change me>";
+CREATE USER "<change me>" WITH INHERIT IN ROLE cwa_ppdd_edus PASSWORD "<change me>";
