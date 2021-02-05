f1dc6a40b8796e33c53e66bfc05b5978ed822f72
FelixRottler <fero429@gmail.com>
2021-02-05 10:59:29 +0100

    feature: add schema to url

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/CallbackFlywayMigrationStrategy.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/CallbackFlywayMigrationStrategy.java
@@ -2,27 +2,27 @@ package app.coronawarn.datadonation.services.ppac.ios;
 
 import java.util.Arrays;
 import java.util.stream.StreamSupport;
+import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TestRunner implements InitializingBean {
+public class CallbackFlywayMigrationStrategy implements FlywayMigrationStrategy {
 
-  private static final Logger logger = LoggerFactory.getLogger(TestRunner.class);
+  private static final Logger logger = LoggerFactory.getLogger(CallbackFlywayMigrationStrategy.class);
 
   @Autowired
   Environment env;
 
   @Override
-  public void afterPropertiesSet() throws Exception {
+  public void migrate(Flyway flyway) {
     logger.info("====== Environment and configuration ======");
     logger.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
 
@@ -35,5 +35,6 @@ public class TestRunner implements InitializingBean {
         .filter(prop -> !(prop.contains("credentials") || prop.contains("password")))
         .forEach(prop -> logger.info("{}: {}", prop, env.getProperty(prop)));
     logger.info("===========================================");
+    flyway.migrate();
   }
 }
