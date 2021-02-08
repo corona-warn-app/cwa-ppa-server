78a3c4dd18162a907ec281fa34e39b021ee338e5
Michael Braun <michael.braun01@sap.com>
2021-02-08 13:37:18 +0100

    Update fields in entity, adapt OTP service

--- common/persistence/src/main/java/app/coronawarn/datadonation/common/persistence/domain/OneTimePassword.java
+++ common/persistence/src/main/java/app/coronawarn/datadonation/common/persistence/domain/OneTimePassword.java
@@ -8,25 +8,52 @@ public class OneTimePassword {
   @Id
   @Size(min = 36, max = 36)
   private String password;
-  private Long creationTimestamp;
   private Long redemptionTimestamp;
-  private Long lastValidityCheckTimestamp;
+  private Long expirationTimestamp;
+  private Boolean androidPpacBasicIntegrity;
+  private Boolean androidPpacCtsProfileMatch;
+  private Boolean androidPpacEvaluationTypeBasic;
+  private Boolean androidPpacEvaluationTypeHardwareBacked;
+  private Boolean androidPpacAdvice;
 
   /**
    * TODO.
-   *
-   * @param password                   a
-   * @param creationTimestamp          b
-   * @param redemptionTimestamp        c
-   * @param lastValidityCheckTimestamp d
+   * @param password The otp to store.
+   * @param redemptionTimestamp The point in time, when the otp was redeemed.
+   * @param expirationTimestamp The point in time, when the otp expires.
    */
   public OneTimePassword(
-      @Size(min = 36, max = 36) String password, Long creationTimestamp,
-      Long redemptionTimestamp, Long lastValidityCheckTimestamp) {
+      @Size(min = 36, max = 36) String password, Long redemptionTimestamp,
+      Long expirationTimestamp) {
     this.password = password;
-    this.creationTimestamp = creationTimestamp;
     this.redemptionTimestamp = redemptionTimestamp;
-    this.lastValidityCheckTimestamp = lastValidityCheckTimestamp;
+    this.expirationTimestamp = expirationTimestamp;
+  }
+
+  /**
+   * TODO.
+   * @param password The otp to store.
+   * @param redemptionTimestamp The point in time, when the otp was redeemed.
+   * @param expirationTimestamp The point in time, when the otp expires.
+   * @param androidPpacBasicIntegrity The Android PPAC Basic Integrity.
+   * @param androidPpacCtsProfileMatch The Android PPAC CTS Profile Match.
+   * @param androidPpacEvaluationTypeBasic Android PPAC Evaluation Type Basic.
+   * @param androidPpacEvaluationTypeHardwareBacked Android PPAC Evaluation Type Hardware-Backed.
+   * @param androidPpacAdvice Android PPAC Advice.
+   */
+  public OneTimePassword(
+      @Size(min = 36, max = 36) String password, Long redemptionTimestamp,
+      Long expirationTimestamp, Boolean androidPpacBasicIntegrity,
+      Boolean androidPpacCtsProfileMatch, Boolean androidPpacEvaluationTypeBasic,
+      Boolean androidPpacEvaluationTypeHardwareBacked, Boolean androidPpacAdvice) {
+    this.password = password;
+    this.redemptionTimestamp = redemptionTimestamp;
+    this.expirationTimestamp = expirationTimestamp;
+    this.androidPpacBasicIntegrity = androidPpacBasicIntegrity;
+    this.androidPpacCtsProfileMatch = androidPpacCtsProfileMatch;
+    this.androidPpacEvaluationTypeBasic = androidPpacEvaluationTypeBasic;
+    this.androidPpacEvaluationTypeHardwareBacked = androidPpacEvaluationTypeHardwareBacked;
+    this.androidPpacAdvice = androidPpacAdvice;
   }
 
   public String getPassword() {
@@ -37,14 +64,6 @@ public class OneTimePassword {
     this.password = password;
   }
 
-  public Long getCreationTimestamp() {
-    return creationTimestamp;
-  }
-
-  public void setCreationTimestamp(Long creationTimestamp) {
-    this.creationTimestamp = creationTimestamp;
-  }
-
   public Long getRedemptionTimestamp() {
     return redemptionTimestamp;
   }
@@ -53,11 +72,52 @@ public class OneTimePassword {
     this.redemptionTimestamp = redemptionTimestamp;
   }
 
-  public Long getLastValidityCheckTimestamp() {
-    return lastValidityCheckTimestamp;
+  public Long getExpirationTimestamp() {
+    return expirationTimestamp;
   }
 
-  public void setLastValidityCheckTimestamp(Long lastValidityCheckTimestamp) {
-    this.lastValidityCheckTimestamp = lastValidityCheckTimestamp;
+  public void setExpirationTimestamp(Long expirationTimestamp) {
+    this.expirationTimestamp = expirationTimestamp;
+  }
+
+  public Boolean getAndroidPpacBasicIntegrity() {
+    return androidPpacBasicIntegrity;
+  }
+
+  public void setAndroidPpacBasicIntegrity(Boolean androidPpacBasicIntegrity) {
+    this.androidPpacBasicIntegrity = androidPpacBasicIntegrity;
+  }
+
+  public Boolean getAndroidPpacCtsProfileMatch() {
+    return androidPpacCtsProfileMatch;
+  }
+
+  public void setAndroidPpacCtsProfileMatch(Boolean androidPpacCtsProfileMatch) {
+    this.androidPpacCtsProfileMatch = androidPpacCtsProfileMatch;
+  }
+
+  public Boolean getAndroidPpacEvaluationTypeBasic() {
+    return androidPpacEvaluationTypeBasic;
+  }
+
+  public void setAndroidPpacEvaluationTypeBasic(Boolean androidPpacEvaluationTypeBasic) {
+    this.androidPpacEvaluationTypeBasic = androidPpacEvaluationTypeBasic;
+  }
+
+  public Boolean getAndroidPpacEvaluationTypeHardwareBacked() {
+    return androidPpacEvaluationTypeHardwareBacked;
+  }
+
+  public void setAndroidPpacEvaluationTypeHardwareBacked(
+      Boolean androidPpacEvaluationTypeHardwareBacked) {
+    this.androidPpacEvaluationTypeHardwareBacked = androidPpacEvaluationTypeHardwareBacked;
+  }
+
+  public Boolean getAndroidPpacAdvice() {
+    return androidPpacAdvice;
+  }
+
+  public void setAndroidPpacAdvice(Boolean androidPpacAdvice) {
+    this.androidPpacAdvice = androidPpacAdvice;
   }
 }
