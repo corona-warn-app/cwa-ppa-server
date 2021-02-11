6c6505f4cb3046bacc0ba4bc32a2499a47485f73
FelixRottler <fero429@gmail.com>
2021-02-11 14:53:51 +0100

    fix: setup ac for db users

--- common/persistence/src/main/resources/db/migration/V1__createTables.sql
+++ common/persistence/src/main/resources/db/migration/V1__createTables.sql
@@ -146,3 +146,24 @@ CREATE TABLE salt (
     salt VARCHAR(32) PRIMARY KEY,
     created_at bigint NOT NULL
 );
+
+-- AC
+GRANT SELECT, INSERT, UPDATE ON TABLE
+  data_donation.api_token,
+  data_donation.device_token,
+  data_donation.exposure_risk_metadata,
+  data_donation.exposure_window,
+  data_donation.key_submission_metadata_with_client_metadata,
+  data_donation.key_submission_metadata_with_user_metadata,
+  data_donation.one_time_password,
+  data_donation.salt,
+  data_donation.scan_instance,
+  data_donation.test_result_metadata
+  TO cwa_ppdd_ppac;
+
+GRANT ALL ON ALL SEQUENCES IN SCHEMA data_donation TO cwa_ppdd_ppac;
+
+GRANT SELECT, UPDATE ON TABLE
+  data_donation.one_time_password
+  TO cwa_ppdd_edus;
+
