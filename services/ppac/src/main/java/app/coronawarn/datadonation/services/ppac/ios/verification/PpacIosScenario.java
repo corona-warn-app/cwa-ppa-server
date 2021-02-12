fbae9639df8669d0b319f408d1170aa31ca9d312
kilbphilippSAP <67139704+kilbphilippSAP@users.noreply.github.com>
2021-02-12 15:19:04 +0100

    Doc/ppac ios scenario javadoc (#70)
    * Javadoc for PpacIosScenario.validate and .save
    
    * Javadoc for PpacIosScenarioRepository
    
    * PpacProcessor javadoc
    
    * Typo

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/PpacIosScenario.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/PpacIosScenario.java
@@ -18,18 +18,22 @@ public enum PpacIosScenario {
   }
 
   /**
-   * TODO.
-   * @param validator TODO.
-   * @param apiToken TODO.
+   * Calls the validate method of the provided Validator with the provided API Token as the input, corresponding to the
+   * current scenario.
+   *
+   * @param validator {@link PpacIosScenarioValidator} which validates the provided API Token.
+   * @param apiToken  {@link ApiToken} that is to be validated.
    */
   public void validate(PpacIosScenarioValidator validator, ApiToken apiToken) {
     this.validator.accept(validator, apiToken);
   }
 
   /**
-   * TODO.
-   * @param repository TODO.
-   * @param apiToken TODO.
+   * Calls the save method provided Repository with the provided API Token String as the input, corresponding to the
+   * current scenario. .
+   *
+   * @param repository {@link PpacIosScenarioRepository} which stores the provided API Token.
+   * @param apiToken   {@link String} of the API Token Key that is to be saved.
    */
   public void save(PpacIosScenarioRepository repository, String apiToken) {
     this.repository.accept(repository, apiToken);
