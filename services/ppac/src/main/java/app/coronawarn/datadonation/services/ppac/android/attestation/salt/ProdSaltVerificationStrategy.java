f1fdd6b063bf8ff297a0ab005479002fbccdf848
Eugen M <eugen.madean@sap.com>
2021-02-16 16:12:27 +0200

    Correctly profile prod bean

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/android/attestation/salt/ProdSaltVerificationStrategy.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/android/attestation/salt/ProdSaltVerificationStrategy.java
@@ -5,22 +5,30 @@ import app.coronawarn.datadonation.common.persistence.repository.ppac.android.Sa
 import app.coronawarn.datadonation.services.ppac.android.attestation.errors.MissingMandatoryAuthenticationFields;
 import app.coronawarn.datadonation.services.ppac.android.attestation.errors.SaltNotValidAnymore;
 import app.coronawarn.datadonation.services.ppac.config.PpacConfiguration;
-import java.time.Instant;
-import org.springframework.stereotype.Component;
 import com.google.common.base.Strings;
+import java.time.Instant;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!loadtest")
 public class ProdSaltVerificationStrategy implements SaltVerificationStrategy {
 
   private final SaltRepository saltRepository;
   private final PpacConfiguration appParameters;
 
+  /**
+   * Just constructs an instance.
+   */
   public ProdSaltVerificationStrategy(SaltRepository saltRepository,
       PpacConfiguration appParameters) {
     this.saltRepository = saltRepository;
     this.appParameters = appParameters;
   }
 
+  /**
+   *  Verify that the given salt has not been redeemed (expired).
+   */
   public void validateSalt(String saltString) {
     if (Strings.isNullOrEmpty(saltString)) {
       throw new MissingMandatoryAuthenticationFields("No salt received");
