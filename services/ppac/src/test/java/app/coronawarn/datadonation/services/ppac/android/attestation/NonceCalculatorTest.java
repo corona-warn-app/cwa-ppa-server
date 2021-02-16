f9941f3693dafb9ad382c85dc4f350dd8583d661
Eugen M <eugen.madean@sap.com>
2021-02-16 15:54:37 +0200

    Additional unit test for nonce calc

--- services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/android/attestation/NonceCalculatorTest.java
+++ services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/android/attestation/NonceCalculatorTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
+import com.google.api.client.util.Base64;
+import com.google.protobuf.InvalidProtocolBufferException;
+import app.coronawarn.datadonation.common.protocols.internal.ppdd.EdusOtp.EDUSOneTimePassword;
 import app.coronawarn.datadonation.services.ppac.android.attestation.errors.NonceCalculationError;
 
 class NonceCalculatorTest {
@@ -28,5 +31,18 @@ class NonceCalculatorTest {
     NonceCalculator calculator = NonceCalculator.of("payload-test-string".getBytes());
     String saltBase64 = calculator.calculate("test-salt-1234");
     assertEquals("M2EqczgxveKiptESiBNRmKqxYv5raTdzyeSZyzsCvjg=", saltBase64);
+  }
+  
+  @Test
+  void shouldComputeCorrectNonceForOTp() throws InvalidProtocolBufferException {
+    //test a precomputed salt string
+    byte[] payload = Base64.decodeBase64("CgtoZWxsby13b3JsZA==");
+    EDUSOneTimePassword otpProto = EDUSOneTimePassword.parseFrom(payload);
+    
+    assertEquals("hello-world", otpProto.getOtp());
+    
+    NonceCalculator calculator = NonceCalculator.of(payload);
+    String saltBase64 = calculator.calculate("Ri0AXC9U+b9hE58VqupI8Q==");
+    assertEquals("ANjVoDcS8v8iQdlNrcxehSggE9WZwIp7VNpjoU7cPsg=", saltBase64);
   }
 }
