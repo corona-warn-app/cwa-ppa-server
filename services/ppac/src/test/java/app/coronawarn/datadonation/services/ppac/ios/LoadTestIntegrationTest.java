d0765e33b97ebd57f7b2f4519b8affedd26535e1
FelixRottler <fero429@gmail.com>
2021-02-17 13:16:30 +0100

    feature: add more validation tests

--- services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/ios/LoadTestIntegrationTest.java
+++ services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/ios/LoadTestIntegrationTest.java
@@ -1,6 +1,21 @@
 package app.coronawarn.datadonation.services.ppac.ios;
 
+import static app.coronawarn.datadonation.common.utils.TimeUtils.getEpochSecondForNow;
+import static app.coronawarn.datadonation.common.utils.TimeUtils.getLastDayOfMonthForNow;
+import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildBase64String;
+import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildDeviceToken;
+import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildIosDeviceData;
+import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildPPADataRequestIosPayload;
+import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildUuid;
+import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.jsonify;
+import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.postSubmission;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 import app.coronawarn.datadonation.common.config.UrlConstants;
+import app.coronawarn.datadonation.common.persistence.domain.ApiToken;
 import app.coronawarn.datadonation.common.persistence.domain.DeviceToken;
 import app.coronawarn.datadonation.common.persistence.repository.ApiTokenRepository;
 import app.coronawarn.datadonation.common.persistence.repository.DeviceTokenRepository;
@@ -12,6 +27,8 @@ import app.coronawarn.datadonation.services.ppac.ios.client.domain.PerDeviceData
 import app.coronawarn.datadonation.services.ppac.ios.verification.JwtProvider;
 import app.coronawarn.datadonation.services.ppac.ios.verification.devicetoken.DeviceTokenRedemptionStrategy;
 import app.coronawarn.datadonation.services.ppac.logging.PpacErrorCode;
+import java.time.OffsetDateTime;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,24 +38,13 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import java.time.OffsetDateTime;
-import java.util.Optional;
-
-import static app.coronawarn.datadonation.common.utils.TimeUtils.getEpochSecondForNow;
-import static app.coronawarn.datadonation.common.utils.TimeUtils.getLastDayOfMonthForNow;
-import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("loadtest")
 public class LoadTestIntegrationTest {
 
   private static final String IOS_SERVICE_URL = UrlConstants.IOS + UrlConstants.DATA;
-  private static final OffsetDateTime OFFSET_DATE_TIME = OffsetDateTime.parse("2021-10-01T10:00:00+01:00");
-
+ 
   @Autowired
   private TestRestTemplate testRestTemplate;
 
@@ -121,26 +127,27 @@ public class LoadTestIntegrationTest {
 
   @Test
   public void testSubmitDataShouldNotThrowApiTokenAlreadyUsed() {
-    // loadtest profile is active so we should NOT throw DEVICE_TOKEN_REDEEMED
+    // Toy ios device data that has last update NOW - this will be compared against current server time
+    // so this means that someone altered the per device data already this month with an api token.
 
-    final String deviceToken = buildBase64String(this.configuration.getIos().getMinDeviceTokenLength() + 1);
-    final String apiToken = buildUuid();
-    final OffsetDateTime now = OffsetDateTime.now();
-    final PerDeviceDataResponse perDeviceDataResponse = buildIosDeviceData(now.minusMonths(1), true);
-    final PPADataRequestIOS submissionPayloadIos = buildPPADataRequestIosPayload(apiToken, deviceToken, false);
-    apiTokenRepository.insert(apiToken, getLastDayOfMonthForNow(), getEpochSecondForNow(), null, null);
+    // given
+    String deviceToken = buildBase64String(this.configuration.getIos().getMinDeviceTokenLength() + 1);
+    String apiToken = buildUuid();
+    PerDeviceDataResponse data = buildIosDeviceData(OffsetDateTime.now(), true);
+    PPADataRequestIOS submissionPayloadIos = buildPPADataRequestIosPayload(apiToken, deviceToken, false);
 
-    when(iosDeviceApiClient.queryDeviceData(any(), any()))
-        .thenReturn(ResponseEntity.ok(jsonify(perDeviceDataResponse)));
-
-    final ResponseEntity<DataSubmissionResponse> responseEntity = postSubmission(submissionPayloadIos, testRestTemplate,
+    // when
+    when(iosDeviceApiClient.queryDeviceData(anyString(), any())).thenReturn(ResponseEntity.ok(jsonify(data)));
+    ResponseEntity<DataSubmissionResponse> response = postSubmission(submissionPayloadIos, testRestTemplate,
         IOS_SERVICE_URL, false);
-    final DeviceToken newDeviceToken = buildDeviceToken(submissionPayloadIos.getAuthentication().getDeviceToken());
-    final Optional<DeviceToken> byDeviceTokenHash = deviceTokenRepository
-        .findByDeviceTokenHash(newDeviceToken.getDeviceTokenHash());
 
-    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-    assertThat(byDeviceTokenHash).isPresent();
+    // then
+    Optional<ApiToken> optionalApiToken = apiTokenRepository.findById(apiToken);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    assertThat(optionalApiToken.isPresent()).isEqualTo(false);
+    assertThat(response.getBody()).isNotNull();
+    assertThat(response.getBody()).isInstanceOf(DataSubmissionResponse.class);
+    assertThat(response.getBody().getErrorCode()).isEqualTo(PpacErrorCode.API_TOKEN_ALREADY_ISSUED);
   }
 
   @Test
