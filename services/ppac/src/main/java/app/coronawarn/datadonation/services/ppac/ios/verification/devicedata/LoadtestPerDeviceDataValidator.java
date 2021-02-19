3d496e613ee1e9a2a440002812bce360b85becd7
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-02-19 15:44:38 +0100

    log exceptions on debug level

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/devicedata/LoadtestPerDeviceDataValidator.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/devicedata/LoadtestPerDeviceDataValidator.java
@@ -6,6 +6,8 @@ import app.coronawarn.datadonation.services.ppac.ios.verification.JwtProvider;
 import app.coronawarn.datadonation.services.ppac.ios.verification.devicetoken.DeviceTokenService;
 import feign.FeignException;
 import feign.FeignException.BadRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -13,19 +15,20 @@ import org.springframework.stereotype.Component;
 @Profile("loadtest")
 public class LoadtestPerDeviceDataValidator extends PerDeviceDataValidator {
 
-  public LoadtestPerDeviceDataValidator(IosDeviceApiClient iosDeviceApiClient,
-      JwtProvider jwtProvider, DeviceTokenService deviceTokenService,
-      PpacConfiguration ppacConfiguration) {
+  private static final Logger logger = LoggerFactory.getLogger(LoadtestPerDeviceDataValidator.class);
+
+  public LoadtestPerDeviceDataValidator(IosDeviceApiClient iosDeviceApiClient, JwtProvider jwtProvider,
+      DeviceTokenService deviceTokenService, PpacConfiguration ppacConfiguration) {
     super(iosDeviceApiClient, jwtProvider, deviceTokenService, ppacConfiguration);
   }
 
   @Override
   protected void treatGeneralRequestError(FeignException e) {
-    // Do nothing during load tests
+    logger.debug(e.getMessage(), e);
   }
 
   @Override
   protected void treatBadRequest(BadRequest e) {
-    // Do nothing during load tests
+    logger.debug(e.getMessage(), e);
   }
 }
