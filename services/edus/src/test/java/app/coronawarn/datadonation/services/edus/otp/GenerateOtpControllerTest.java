c110cd0233a155ed7203604ee12df852556d9054
Philipp Kilb <philipp.kilb@sap.com>
2021-03-01 09:23:11 +0100

    Add tests for GenerateOtpController

--- services/edus/src/test/java/app/coronawarn/datadonation/services/edus/otp/GenerateOtpControllerTest.java
+++ services/edus/src/test/java/app/coronawarn/datadonation/services/edus/otp/GenerateOtpControllerTest.java
@@ -1,12 +1,7 @@
 package app.coronawarn.datadonation.services.edus.otp;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import app.coronawarn.datadonation.common.persistence.service.OtpService;
 import app.coronawarn.datadonation.common.persistence.service.OtpTestGenerationResponse;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
@@ -14,33 +9,28 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("generate-otp")
 @DirtiesContext
 public class GenerateOtpControllerTest {
-
-  @MockBean
-  OtpService otpService;
 
   @Autowired
   GenerateOtpController generateOtpController;
 
   @Test
   void testOtpsAreCreated() {
-    int numberOfInvocations = 10;
-    int validityInHours = 4;
-
-    when(otpService.createOtp(any(), validityInHours))
-        .thenReturn(ZonedDateTime.now(ZoneOffset.UTC).plusHours(validityInHours));
+    int numberOfInvocations = 15;
+    int validityInHours = 5;
 
     List<OtpTestGenerationResponse> responses = generateOtpController.generateOtp(numberOfInvocations, validityInHours)
         .getBody();
 
-    verify(otpService, times(numberOfInvocations)).createOtp(any(), any());
-
     assert responses != null;
+    assertThat(responses.size()).isEqualTo(numberOfInvocations);
+
     for (OtpTestGenerationResponse response : responses) {
       assertThat(ZonedDateTime.now(ZoneOffset.UTC).plusHours(validityInHours))
           .isEqualToIgnoringSeconds(response.getExpirationDate());
