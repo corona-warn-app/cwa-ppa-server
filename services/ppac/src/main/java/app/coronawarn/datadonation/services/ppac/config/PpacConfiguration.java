57960cbf8e698579cb6944aa0903c75dc8a077ec
Sorin Stefan Iovita <sorin.stefan.iovita@sap.com>
2021-03-03 18:49:11 +0200

    refactor ppac configuration to avoid code duplication

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/config/PpacConfiguration.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/config/PpacConfiguration.java
@@ -11,7 +11,7 @@ public class PpacConfiguration {
 
   public static final class Android {
 
-    public static final class Dat {
+    private static class CommonAndroidProperties {
 
       @NotNull
       private Boolean requireBasicIntegrity;
@@ -55,52 +55,15 @@ public class PpacConfiguration {
       }
     }
 
-    public static final class Otp {
+    public static final class Dat extends CommonAndroidProperties {
 
-      @NotNull
-      private Boolean requireBasicIntegrity;
-      @NotNull
-      private Boolean requireCtsProfileMatch;
-      @NotNull
-      private Boolean requireEvaluationTypeBasic;
-      @NotNull
-      private Boolean requireEvaluationTypeHardwareBacked;
+    }
 
-      public Boolean getRequireBasicIntegrity() {
-        return requireBasicIntegrity;
-      }
+    public static final class Otp extends CommonAndroidProperties {
 
-      public void setRequireBasicIntegrity(Boolean requireBasicIntegrity) {
-        this.requireBasicIntegrity = requireBasicIntegrity;
-      }
-
-      public Boolean getRequireCtsProfileMatch() {
-        return requireCtsProfileMatch;
-      }
-
-      public void setRequireCtsProfileMatch(Boolean requireCtsProfileMatch) {
-        this.requireCtsProfileMatch = requireCtsProfileMatch;
-      }
-
-      public Boolean getRequireEvaluationTypeBasic() {
-        return requireEvaluationTypeBasic;
-      }
-
-      public void setRequireEvaluationTypeBasic(Boolean requireEvaluationTypeBasic) {
-        this.requireEvaluationTypeBasic = requireEvaluationTypeBasic;
-      }
-
-      public Boolean getRequireEvaluationTypeHardwareBacked() {
-        return requireEvaluationTypeHardwareBacked;
-      }
-
-      public void setRequireEvaluationTypeHardwareBacked(Boolean requireEvaluationTypeHardwareBacked) {
-        this.requireEvaluationTypeHardwareBacked = requireEvaluationTypeHardwareBacked;
-      }
     }
 
     private Log log;
-
     private Dat dat;
     private Otp otp;
 
@@ -112,48 +75,8 @@ public class PpacConfiguration {
       this.log = log;
     }
 
-    public static final class Log {
+    public static final class Log extends CommonAndroidProperties {
 
-      @NotNull
-      private Boolean requireBasicIntegrity;
-      @NotNull
-      private Boolean requireCtsProfileMatch;
-      @NotNull
-      private Boolean requireEvaluationTypeBasic;
-      @NotNull
-      private Boolean requireEvaluationTypeHardwareBacked;
-
-      public Boolean getRequireBasicIntegrity() {
-        return requireBasicIntegrity;
-      }
-
-      public void setRequireBasicIntegrity(Boolean requireBasicIntegrity) {
-        this.requireBasicIntegrity = requireBasicIntegrity;
-      }
-
-      public Boolean getRequireCtsProfileMatch() {
-        return requireCtsProfileMatch;
-      }
-
-      public void setRequireCtsProfileMatch(Boolean requireCtsProfileMatch) {
-        this.requireCtsProfileMatch = requireCtsProfileMatch;
-      }
-
-      public Boolean getRequireEvaluationTypeBasic() {
-        return requireEvaluationTypeBasic;
-      }
-
-      public void setRequireEvaluationTypeBasic(Boolean requireEvaluationTypeBasic) {
-        this.requireEvaluationTypeBasic = requireEvaluationTypeBasic;
-      }
-
-      public Boolean getRequireEvaluationTypeHardwareBacked() {
-        return requireEvaluationTypeHardwareBacked;
-      }
-
-      public void setRequireEvaluationTypeHardwareBacked(Boolean requireEvaluationTypeHardwareBacked) {
-        this.requireEvaluationTypeHardwareBacked = requireEvaluationTypeHardwareBacked;
-      }
     }
 
     @NotEmpty
