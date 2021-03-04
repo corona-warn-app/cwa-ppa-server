6ec03ed96ea9a458c514541017437d24e12bd123
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-03-04 10:34:20 +0100

    treat android_enf_version + android_api_level as long (#150)
    * obsolete files
    
    * android_api_level + android_enf_version LONG
    
    * fix tests

--- common/persistence/src/test/java/app/coronawarn/datadonation/common/persistence/repository/metrics/ExposureWindowRepositoryTest.java
+++ common/persistence/src/test/java/app/coronawarn/datadonation/common/persistence/repository/metrics/ExposureWindowRepositoryTest.java
@@ -31,26 +31,23 @@ class ExposureWindowRepositoryTest {
   @Test
   void exposureWindowShouldBePersistedCorrectly() {
     LocalDate justADate = LocalDate.now(ZoneId.of("UTC"));
-    ClientMetadataDetails clientMetadata = new ClientMetadataDetails(1, 1, 1, "abc", 2, 2, 3, 1, 2);
-    TechnicalMetadata technicalMetadata =
-        new TechnicalMetadata(justADate, true, false, true, false);
-    Set<ScanInstance> scanInstances =
-        Set.of(new ScanInstance(null, null, 5, 4, 2), new ScanInstance(null, null, 7, 7, 7));
-    ExposureWindow exposureMetrics =
-        new ExposureWindow(null, justADate, 1, 1, 1, 2, 2.23, clientMetadata, technicalMetadata, scanInstances);
+    ClientMetadataDetails clientMetadata = new ClientMetadataDetails(1, 1, 1, "abc", 2, 2, 3, 1l, 2l);
+    TechnicalMetadata technicalMetadata = new TechnicalMetadata(justADate, true, false, true, false);
+    Set<ScanInstance> scanInstances = Set.of(new ScanInstance(null, null, 5, 4, 2),
+        new ScanInstance(null, null, 7, 7, 7));
+    ExposureWindow exposureMetrics = new ExposureWindow(null, justADate, 1, 1, 1, 2, 2.23, clientMetadata,
+        technicalMetadata, scanInstances);
 
     exposureWindowRepository.save(exposureMetrics);
     ExposureWindow loadedEntity = exposureWindowRepository.findAll().iterator().next();
-    assertEquals(loadedEntity.getCallibrationConfidence(),
-        exposureMetrics.getCallibrationConfidence());
+    assertEquals(loadedEntity.getCallibrationConfidence(), exposureMetrics.getCallibrationConfidence());
     assertEquals(loadedEntity.getClientMetadata(), exposureMetrics.getClientMetadata());
     assertEquals(loadedEntity.getDate(), exposureMetrics.getDate());
     assertEquals(loadedEntity.getInfectiousness(), exposureMetrics.getInfectiousness());
     assertEquals(loadedEntity.getNormalizedTime(), exposureMetrics.getNormalizedTime());
     assertEquals(loadedEntity.getReportType(), exposureMetrics.getReportType());
     assertEquals(loadedEntity.getTechnicalMetadata(), exposureMetrics.getTechnicalMetadata());
-    assertEquals(loadedEntity.getTransmissionRiskLevel(),
-        exposureMetrics.getTransmissionRiskLevel());
+    assertEquals(loadedEntity.getTransmissionRiskLevel(), exposureMetrics.getTransmissionRiskLevel());
     assertNotNull(loadedEntity.getId());
     assertScanInstancesDataAreEqual(scanInstances, loadedEntity.getScanInstances());
   }
@@ -60,21 +57,16 @@ class ExposureWindowRepositoryTest {
     ArrayList<ScanInstance> beforePersistenceScans = new ArrayList<>(beforePersistence);
     ArrayList<ScanInstance> afterPersistenceScans = new ArrayList<>(afterPersistence);
     beforePersistenceScans.sort(Comparator.comparingInt(ScanInstance::getMinimumAttenuation)
-        .thenComparingInt(ScanInstance::getTypicalAttenuation)
-        .thenComparingInt(ScanInstance::getSecondsSinceLastScan));
+        .thenComparingInt(ScanInstance::getTypicalAttenuation).thenComparingInt(ScanInstance::getSecondsSinceLastScan));
     afterPersistenceScans.sort(Comparator.comparingInt(ScanInstance::getMinimumAttenuation)
-        .thenComparingInt(ScanInstance::getTypicalAttenuation)
-        .thenComparingInt(ScanInstance::getSecondsSinceLastScan));
+        .thenComparingInt(ScanInstance::getTypicalAttenuation).thenComparingInt(ScanInstance::getSecondsSinceLastScan));
 
     for (int i = 0; i < beforePersistenceScans.size(); i++) {
       ScanInstance beforePersistenceScan = beforePersistenceScans.get(i);
       ScanInstance afterPersistenceScan = afterPersistenceScans.get(i);
-      if (beforePersistenceScan.getMinimumAttenuation() != afterPersistenceScan
-          .getMinimumAttenuation()
-          || beforePersistenceScan.getTypicalAttenuation() != afterPersistenceScan
-              .getTypicalAttenuation()
-          || beforePersistenceScan.getSecondsSinceLastScan() != afterPersistenceScan
-              .getSecondsSinceLastScan()) {
+      if (beforePersistenceScan.getMinimumAttenuation() != afterPersistenceScan.getMinimumAttenuation()
+          || beforePersistenceScan.getTypicalAttenuation() != afterPersistenceScan.getTypicalAttenuation()
+          || beforePersistenceScan.getSecondsSinceLastScan() != afterPersistenceScan.getSecondsSinceLastScan()) {
         throw new AssertionError("scan instances were not persisted correctly");
       }
     }
