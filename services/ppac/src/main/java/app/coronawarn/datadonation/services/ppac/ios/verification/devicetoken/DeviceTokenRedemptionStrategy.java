d9257e904e1644af790b6975d0135952364c334f
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-03-04 10:26:28 +0100

    SecurityLogger::securityWarn (#149)
    * SecurityLogger::securityWarn
    
    * use java.util.Base64 instead of com.google.api.client.util.Base64
    
    * throws InternalError
    
    * shouldComputeCorrectNonce is an invalid test
    
    * checkstlye
    
    * is null or empty
    
    * regular warn has become obsolete
    
    * don't log stacktraces on WARNING level
    
    Co-authored-by: Sorin Stefan Iovita <sorin.stefan.iovita@sap.com>

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/devicetoken/DeviceTokenRedemptionStrategy.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/devicetoken/DeviceTokenRedemptionStrategy.java
@@ -2,6 +2,12 @@ package app.coronawarn.datadonation.services.ppac.ios.verification.devicetoken;
 
 public interface DeviceTokenRedemptionStrategy {
 
-  void redeem(Exception e);
+  /**
+   * How to handle exceptions during OTP redemption.
+   *
+   * @param e {@link Exception} to be handled
+   * @throws InternalError in case redemption fails
+   */
+  void redeem(Exception e) throws InternalError;
 
 }
--- services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/android/attestation/NonceCalculatorTest.java
+++ services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/android/attestation/NonceCalculatorTest.java
@@ -3,25 +3,26 @@ package app.coronawarn.datadonation.services.ppac.android.attestation;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.junit.jupiter.api.Test;
-import com.google.api.client.util.Base64;
-import com.google.protobuf.InvalidProtocolBufferException;
+
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.EDUSOneTimePassword;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.ExposureRiskMetadata;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPADataAndroid;
-import app.coronawarn.datadonation.services.ppac.android.attestation.errors.NonceCalculationError;
+import app.coronawarn.datadonation.services.ppac.android.attestation.errors.NonceCouldNotBeVerified;
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.util.Base64;
 import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class NonceCalculatorTest {
 
   @Test
   void shouldThrowExceptionForMissingOrInvalidArguments() {
-    NonceCalculationError exception = assertThrows(NonceCalculationError.class, () -> {
+    NonceCouldNotBeVerified exception = assertThrows(NonceCouldNotBeVerified.class, () -> {
       NonceCalculator.of(null);
     });
     assertFalse(exception.getMessage().isEmpty());
 
-    exception = assertThrows(NonceCalculationError.class, () -> {
+    exception = assertThrows(NonceCouldNotBeVerified.class, () -> {
       NonceCalculator calculator = NonceCalculator.of("payload".getBytes());
       calculator.calculate(null);
     });
@@ -29,35 +30,27 @@ class NonceCalculatorTest {
   }
 
   @Test
-  void shouldComputeCorrectNonce() {
-    //test a precomputed salt string
-    NonceCalculator calculator = NonceCalculator.of("payload-test-string".getBytes());
-    String saltBase64 = calculator.calculate("test-salt-1234");
-    assertEquals("M2EqczgxveKiptESiBNRmKqxYv5raTdzyeSZyzsCvjg=", saltBase64);
-  }
-  
-  @Test
   void shouldComputeCorrectNonceForOTp() throws InvalidProtocolBufferException {
-    //test a precomputed salt string
-    byte[] payload = Base64.decodeBase64("CgtoZWxsby13b3JsZA==");
+    // test a precomputed salt string
+    byte[] payload = Base64.getDecoder().decode("CgtoZWxsby13b3JsZA==");
     EDUSOneTimePassword otpProto = EDUSOneTimePassword.parseFrom(payload);
-    
+
     assertEquals("hello-world", otpProto.getOtp());
-    
+
     NonceCalculator calculator = NonceCalculator.of(payload);
     String saltBase64 = calculator.calculate("Ri0AXC9U+b9hE58VqupI8Q==");
     assertEquals("ANjVoDcS8v8iQdlNrcxehSggE9WZwIp7VNpjoU7cPsg=", saltBase64);
   }
-  
+
   @Test
   void shouldComputeCorrectNonceForPpa() throws InvalidProtocolBufferException {
-    //test a precomputed salt string
-    byte[] payload = Base64.decodeBase64("Eg0IAxABGMGFyOT6LiABOgkIBBDdj6AFGAI=");
+    // test a precomputed salt string
+    byte[] payload = Base64.getDecoder().decode("Eg0IAxABGMGFyOT6LiABOgkIBBDdj6AFGAI=");
     PPADataAndroid dataProto = PPADataAndroid.parseFrom(payload);
 
     List<ExposureRiskMetadata> exposureRiskMetadata = dataProto.getExposureRiskMetadataSetList();
     assertEquals(exposureRiskMetadata.get(0).getRiskLevelValue(), 3);
-    
+
     NonceCalculator calculator = NonceCalculator.of(payload);
     String saltBase64 = calculator.calculate("Ri0AXC9U+b9hE58VqupI8Q==");
     assertEquals("bd6kMfLKby3pzEqW8go1ZgmHN/bU1p/4KG6+1GeB288=", saltBase64);
