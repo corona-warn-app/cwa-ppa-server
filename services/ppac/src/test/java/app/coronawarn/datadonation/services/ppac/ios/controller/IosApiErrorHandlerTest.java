b9974cd727ac5a7da9ef92238a08bb9322f2ffad
Jeremias Rößler <jeremias.roessler@sap.com>
2021-03-16 10:38:06 +0100

    Massively improved and simplified test

--- services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/ios/controller/IosApiErrorHandlerTest.java
+++ services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/ios/controller/IosApiErrorHandlerTest.java
@@ -1,35 +1,30 @@
 package app.coronawarn.datadonation.services.ppac.ios.controller;
 
+import static app.coronawarn.datadonation.common.utils.TimeUtils.getEpochSecondFor;
+import static app.coronawarn.datadonation.common.utils.TimeUtils.getLastDayOfMonthFor;
 import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildBase64String;
-import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildDeviceToken;
 import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildIosDeviceData;
 import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildPPADataRequestIosPayload;
 import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildUuid;
 import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.jsonify;
 import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.postSubmission;
-import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.postSurvey;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import app.coronawarn.datadonation.common.config.UrlConstants;
-import app.coronawarn.datadonation.common.persistence.domain.ApiToken;
-import app.coronawarn.datadonation.common.persistence.domain.DeviceToken;
 import app.coronawarn.datadonation.common.persistence.repository.ApiTokenRepository;
-import app.coronawarn.datadonation.common.persistence.repository.DeviceTokenRepository;
-import app.coronawarn.datadonation.common.protocols.internal.ppdd.EDUSOneTimePasswordRequestIOS;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPADataRequestIOS;
 import app.coronawarn.datadonation.services.ppac.commons.web.DataSubmissionResponse;
 import app.coronawarn.datadonation.services.ppac.config.PpacConfiguration;
 import app.coronawarn.datadonation.services.ppac.ios.client.IosDeviceApiClient;
 import app.coronawarn.datadonation.services.ppac.ios.client.domain.PerDeviceDataResponse;
-import app.coronawarn.datadonation.services.ppac.ios.testdata.TestData;
 import app.coronawarn.datadonation.services.ppac.ios.verification.JwtProvider;
 import app.coronawarn.datadonation.services.ppac.logging.PpacErrorCode;
 import java.time.OffsetDateTime;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,16 +42,10 @@ public class IosApiErrorHandlerTest {
   // Then throws a ClientAbortException during exception handling
   // then see what happens...
 
-
   private static final String IOS_SERVICE_URL = UrlConstants.IOS + UrlConstants.DATA;
-  private static final String IOS_SURVEY_URL = UrlConstants.IOS + UrlConstants.OTP;
-  private static final OffsetDateTime OFFSET_DATE_TIME = OffsetDateTime.parse("2021-10-01T10:00:00+01:00");
 
   @Autowired
   private TestRestTemplate testRestTemplate;
-
-  @Autowired
-  private DeviceTokenRepository deviceTokenRepository;
 
   @Autowired
   private PpacConfiguration configuration;
@@ -75,52 +64,37 @@ public class IosApiErrorHandlerTest {
 
   @BeforeEach
   void clearDatabase() {
-    deviceTokenRepository.deleteAll();
     apiTokenRepository.deleteAll();
     when(jwtProvider.generateJwt()).thenReturn("jwt");
   }
 
   @Test
-  public void checkExceptionsInExceptionhandlerAreCaughtAndStatusCode500IsReturnedInResponse() {
-    final String deviceToken = buildBase64String(this.configuration.getIos().getMinDeviceTokenLength() + 1);
-    final String deviceTokenForSurvey = buildBase64String(this.configuration.getIos().getMinDeviceTokenLength() + 2);
-    final String apiToken = buildUuid();
-    final String otp = buildUuid();
-    final OffsetDateTime now = OffsetDateTime.now();
-    final PerDeviceDataResponse perDeviceDataResponse = buildIosDeviceData(now.minusMonths(1), true);
-    final PPADataRequestIOS submissionPayloadIos = buildPPADataRequestIosPayload(apiToken, deviceToken, false);
-    final EDUSOneTimePasswordRequestIOS edusOneTimePasswordRequestIOS = TestData
-        .buildEdusOneTimePasswordPayload(apiToken, deviceTokenForSurvey, otp);
+  public void testSubmitDataShouldNotThrowApiTokenQuotaExceeded() {
+    // Given a valid device Token and an existing ApiToken we need to check if
+    // this api token was already used today for PPA.
+    // If so then NORMALLY there would be a API_TOKEN_QUOTA_EXCEEDED
+    String deviceToken = buildBase64String(this.configuration.getIos().getMinDeviceTokenLength() + 1);
+    String apiToken = buildUuid();
+    PerDeviceDataResponse data = buildIosDeviceData(OffsetDateTime.now(), true);
+    PPADataRequestIOS submissionPayloadIos = buildPPADataRequestIosPayload(apiToken, deviceToken, false);
+    OffsetDateTime now = OffsetDateTime.now();
+    Long expirationDate = getLastDayOfMonthFor(now);
+    long timestamp = getEpochSecondFor(now);
+    apiTokenRepository.insert(apiToken, expirationDate, expirationDate, timestamp, timestamp);
 
-    when(iosDeviceApiClient.queryDeviceData(any(), any()))
-        .thenReturn(ResponseEntity.ok(jsonify(perDeviceDataResponse)));
+    // Triggering the ClientAbortException is not easy, as this is pretty low-level in the call stack
+    // For more info, see https://stackoverflow.com/a/38733497/58997
+    // Instead, set a breakpoint in org.apache.catalina.connector.OutputBuffer.doFlush(OutputBuffer.java:305)
+    // and manually trigger the exception in the debugger:
+    // IntelliJ: Right Click on the StackFrame -> Throw Exception
 
-    final ResponseEntity<DataSubmissionResponse> responseEntity = postSubmission(submissionPayloadIos, testRestTemplate,
+    when(iosDeviceApiClient.queryDeviceData(anyString(), any())).thenReturn(ResponseEntity.ok(jsonify(data)));
+    ResponseEntity<DataSubmissionResponse> response = postSubmission(submissionPayloadIos, testRestTemplate,
         IOS_SERVICE_URL, false);
-    final ResponseEntity<DataSubmissionResponse> surveyResponseEntity = postSurvey(edusOneTimePasswordRequestIOS,
-        testRestTemplate,
-        IOS_SURVEY_URL, false);
 
-    final DeviceToken newDeviceToken = buildDeviceToken(submissionPayloadIos.getAuthentication().getDeviceToken());
-    final DeviceToken newSurveyDeviceToken = buildDeviceToken(
-        edusOneTimePasswordRequestIOS.getAuthentication().getDeviceToken());
-
-    final Optional<DeviceToken> byDeviceTokenHash = deviceTokenRepository
-        .findByDeviceTokenHash(newDeviceToken.getDeviceTokenHash());
-    final Optional<DeviceToken> surveyDeviceToken = deviceTokenRepository
-        .findByDeviceTokenHash(newSurveyDeviceToken.getDeviceTokenHash());
-    final Optional<ApiToken> apiTokenOptional = apiTokenRepository.findById(apiToken);
-
-    final String secondDeviceTokenForSurvey = buildBase64String(
-        this.configuration.getIos().getMinDeviceTokenLength() + 3);
-    final EDUSOneTimePasswordRequestIOS secondEdusOneTimePasswordRequestIOS = TestData
-        .buildEdusOneTimePasswordPayload(apiToken, secondDeviceTokenForSurvey, otp);
-
-    final ResponseEntity<DataSubmissionResponse> errorResponse = postSurvey(secondEdusOneTimePasswordRequestIOS,
-        testRestTemplate,
-        IOS_SURVEY_URL, false);
-    assertThat(errorResponse.getStatusCode()).isEqualTo(HttpStatus.TOO_MANY_REQUESTS);
-    assertThat(errorResponse.getBody().getErrorCode()).isEqualTo(PpacErrorCode.API_TOKEN_QUOTA_EXCEEDED);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.TOO_MANY_REQUESTS);
+    assertThat(response.getBody().getErrorCode()).isEqualTo(PpacErrorCode.API_TOKEN_QUOTA_EXCEEDED);
     verify(iosApiErrorHandler, times(1)).handleTooManyRequestsErrors(any(), any());
+
   }
 }
