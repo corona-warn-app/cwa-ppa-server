52e459445dc5f5f7cdb79d993fd3160f7abce99a
Jeremias Rößler <jeremias.roessler@sap.com>
2021-03-18 12:20:52 +0100

    Split validation to before contacting Apple in case of existing ApiTokens or after contacting Apple for new ApiTokens

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/PpacProcessor.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/PpacProcessor.java
@@ -31,10 +31,10 @@ public class PpacProcessor {
 
   /**
    * Incoming data submission requests must be validated before further processing. This means that it must be ensured
-   * that the request was indeed coming from a valid CWA client. The first step is to validate the DeviceToken {@link
-   * DeviceToken} against the Apple DeviceCheck API. Valid DeviceToken's are then stored to prevent replay attacks.
-   * Second step is to validate the provided ApiToken {@link ApiToken} and to update the corresponding per-Device Data
-   * (if existing or creating a new one).
+   * that the request was indeed coming from a valid CWA client. The first step is to validate the provided ApiToken
+   * {@link ApiToken}. Second step is to validate the DeviceToken {@link DeviceToken} against the Apple DeviceCheck API.
+   * Valid DeviceToken's are then stored to prevent replay attacks. Last step is to update the corresponding per-Device
+   * Data (if existing or creating a new one).
    *
    * @param authentication              authentication object that contains the device token and the API token.
    * @param ignoreApiTokenAlreadyIssued flag to indicate whether the ApiToken should be validated
@@ -43,6 +43,7 @@ public class PpacProcessor {
    */
   public void validate(PPACIOS authentication, final boolean ignoreApiTokenAlreadyIssued,
       PpacScenario scenario) {
+    apiTokenService.validateLocally(authentication, scenario);
     String transactionId = UUID.randomUUID().toString();
     PerDeviceDataResponse perDeviceDataResponse = perDeviceDataValidator
         .validateAndStoreDeviceToken(transactionId, authentication.getDeviceToken());
