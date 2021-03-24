213d17bd5eeb342f527f5c7a55d48d432feeccbf
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-03-24 13:08:07 +0100

    mofified message

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/errors/ExternalServiceError.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/errors/ExternalServiceError.java
@@ -2,10 +2,10 @@ package app.coronawarn.datadonation.services.ppac.ios.verification.errors;
 
 import feign.FeignException;
 
+@SuppressWarnings("serial")
 public class ExternalServiceError extends InternalServerError {
 
   public ExternalServiceError(FeignException cause) {
-    super("Exception contacting external service occurred with status " + cause.status() + " and response "
-        + cause.contentUTF8() + " to request " + cause.request(), cause);
+    super("'" + cause.request() + "' responded with HTTP-" + cause.status() + ": '" + cause.contentUTF8() + "'", cause);
   }
 }
