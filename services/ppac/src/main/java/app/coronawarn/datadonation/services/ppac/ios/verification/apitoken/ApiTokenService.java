9eaf4acc24bf073110b12e33067e76a6d80ee5b3
Jeremias Rößler <jeremias.roessler@sap.com>
2021-03-24 11:00:51 +0100

    Feature/change validation order (#179)
    * Split validation to before contacting Apple in case of existing ApiTokens or after contacting Apple for new ApiTokens
    
    * Small improvements spotted while implementing the validation split
    
    * Improve existing test
    
    * Add test that verifies the behavior and fails on main

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/apitoken/ApiTokenService.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/apitoken/ApiTokenService.java
@@ -14,6 +14,7 @@ import app.coronawarn.datadonation.services.ppac.ios.verification.PpacIosScenari
 import app.coronawarn.datadonation.services.ppac.ios.verification.apitoken.authentication.ApiTokenAuthenticationStrategy;
 import app.coronawarn.datadonation.services.ppac.ios.verification.errors.ApiTokenAlreadyUsed;
 import app.coronawarn.datadonation.services.ppac.ios.verification.errors.ApiTokenExpired;
+import app.coronawarn.datadonation.services.ppac.ios.verification.errors.ApiTokenQuotaExceeded;
 import app.coronawarn.datadonation.services.ppac.ios.verification.errors.InternalError;
 import app.coronawarn.datadonation.services.ppac.ios.verification.scenario.ratelimit.PpacIosRateLimitStrategy;
 import feign.FeignException;
@@ -52,17 +53,15 @@ public abstract class ApiTokenService {
   }
 
   /**
-   * Authenticate an incoming requests against the following constraints. If the provided ApiToken {@link ApiToken} does
+   * Authenticate an incoming request against the following constraints. If the provided ApiToken {@link ApiToken} does
    * not exist. Check if the corresponding per-Device Data (if exists) and compares when it was last updated. If equals
    * to the same month this means that the ApiToken was already used this month to update the per-device Data. If not it
-   * is safe to update the corresponding per-Device Data. If the provided ApiToken does already exist its expiration
-   * data is checked.
+   * is safe to update the corresponding per-Device Data.
    *
    * @param perDeviceDataResponse       per-device Data associated to the ApiToken.
    * @param transactionId               a valid transaction Id.
    * @param ignoreApiTokenAlreadyIssued flag to indicate whether the ApiToken should be validated against the last
    *                                    updated time from the per-device Data.
-   * @throws ApiTokenExpired     - in case the ApiToken already expired.
    * @throws ApiTokenAlreadyUsed - in case the ApiToken was already issued this month.
    * @throws InternalError       - in case updating the per-device Data was not successful.
    */
@@ -74,14 +73,32 @@ public abstract class ApiTokenService {
       boolean ignoreApiTokenAlreadyIssued,
       PpacScenario ppacScenario) {
     Optional<ApiToken> apiTokenOptional = apiTokenRepository.findById(ppacios.getApiToken());
-    if (apiTokenOptional.isPresent()) {
-      this.authenticateExistingApiToken(apiTokenOptional.get(), ppacScenario);
-    } else {
+    if (!apiTokenOptional.isPresent()) {
       this.authenticateNewApiToken(perDeviceDataResponse,
           ppacios,
           transactionId,
           ignoreApiTokenAlreadyIssued,
           ppacScenario);
+    }
+  }
+
+  /**
+   * Authenticate an incoming request if the provided ApiToken does already exist.
+   * Then its expiration data is checked as well as the rate limit.
+   *
+   * @param ppacios                   the client request.
+   * @param ppacScenario              the scenario we are currently in.
+   *
+   * @throws ApiTokenExpired          - in case the ApiToken already expired.
+   * @throws ApiTokenQuotaExceeded    - in case the client has run into the rate limit.
+   */
+  @Transactional
+  public void validateLocally(
+      PPACIOS ppacios,
+      PpacScenario ppacScenario) {
+    Optional<ApiToken> apiTokenOptional = apiTokenRepository.findById(ppacios.getApiToken());
+    if (apiTokenOptional.isPresent()) {
+      authenticateExistingApiToken(apiTokenOptional.get(), ppacScenario);
     }
   }
 
--- services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/ios/verification/PpacProcessorIntegrationTest.java
+++ services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/ios/verification/PpacProcessorIntegrationTest.java
@@ -1,30 +1,26 @@
-package app.coronawarn.datadonation.services.ppac.ios.controller;
+package app.coronawarn.datadonation.services.ppac.ios.verification;
 
 import static app.coronawarn.datadonation.common.utils.TimeUtils.getEpochSecondFor;
 import static app.coronawarn.datadonation.common.utils.TimeUtils.getLastDayOfMonthFor;
-import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildBase64String;
-import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildIosDeviceData;
-import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildPPADataRequestIosPayload;
-import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildUuid;
-import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.jsonify;
-import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.postSubmission;
+import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import app.coronawarn.datadonation.common.config.UrlConstants;
+import app.coronawarn.datadonation.common.persistence.domain.ApiToken;
 import app.coronawarn.datadonation.common.persistence.repository.ApiTokenRepository;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPADataRequestIOS;
 import app.coronawarn.datadonation.services.ppac.commons.web.DataSubmissionResponse;
 import app.coronawarn.datadonation.services.ppac.config.PpacConfiguration;
 import app.coronawarn.datadonation.services.ppac.ios.client.IosDeviceApiClient;
 import app.coronawarn.datadonation.services.ppac.ios.client.domain.PerDeviceDataResponse;
-import app.coronawarn.datadonation.services.ppac.ios.verification.JwtProvider;
 import app.coronawarn.datadonation.services.ppac.logging.PpacErrorCode;
 import java.time.OffsetDateTime;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,15 +30,13 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class IosApiErrorHandlerTest {
-
-  // Create a setup that throws an ApiTokenQuotaExceeded during processing
-  // Then throws a ClientAbortException during exception handling
-  // then see what happens...
+public class PpacProcessorIntegrationTest {
 
   private static final String IOS_SERVICE_URL = UrlConstants.IOS + UrlConstants.DATA;
+  private static final OffsetDateTime OFFSET_DATE_TIME = OffsetDateTime.parse("2021-10-01T10:00:00+01:00");
 
   @Autowired
   private TestRestTemplate testRestTemplate;
@@ -56,9 +50,6 @@ public class IosApiErrorHandlerTest {
   @MockBean
   private JwtProvider jwtProvider;
 
-  @SpyBean
-  IosApiErrorHandler iosApiErrorHandler;
-
   @Autowired
   ApiTokenRepository apiTokenRepository;
 
@@ -66,34 +57,45 @@ public class IosApiErrorHandlerTest {
   void clearDatabase() {
     apiTokenRepository.deleteAll();
     when(jwtProvider.generateJwt()).thenReturn("jwt");
+    PerDeviceDataResponse data = buildIosDeviceData(OFFSET_DATE_TIME, true);
+    when(iosDeviceApiClient.queryDeviceData(anyString(), any())).thenReturn(ResponseEntity.ok(jsonify(data)));
   }
 
   @Test
-  public void testApiTokenQuotaExceeded_with_manual_ClientAbortException() {
-    // Given a valid device Token and an existing ApiToken we need to check if
-    // this api token was already used today for PPA.
-    // If so then NORMALLY there would be a API_TOKEN_QUOTA_EXCEEDED
+  public void testApiTokenQuotaExceededShouldNotTriggerAppleCall() {
     String deviceToken = buildBase64String(this.configuration.getIos().getMinDeviceTokenLength() + 1);
     String apiToken = buildUuid();
-    PerDeviceDataResponse data = buildIosDeviceData(OffsetDateTime.now(), true);
-    PPADataRequestIOS submissionPayloadIos = buildPPADataRequestIosPayload(apiToken, deviceToken, false);
     OffsetDateTime now = OffsetDateTime.now();
+    PerDeviceDataResponse data = buildIosDeviceData(now, true);
+    PPADataRequestIOS submissionPayloadIos = buildPPADataRequestIosPayload(apiToken, deviceToken, false);
     Long expirationDate = getLastDayOfMonthFor(now);
     long timestamp = getEpochSecondFor(now);
     apiTokenRepository.insert(apiToken, expirationDate, expirationDate, timestamp, timestamp);
 
-    // Triggering the ClientAbortException is not easy, as this is pretty low-level in the call stack
-    // For more info, see https://stackoverflow.com/a/38733497/58997
-    // Instead, set a breakpoint in org.apache.catalina.connector.OutputBuffer.doFlush(OutputBuffer.java:305)
-    // and manually trigger the exception in the debugger:
-    // IntelliJ: Right Click on the StackFrame -> Throw Exception
-
-    when(iosDeviceApiClient.queryDeviceData(anyString(), any())).thenReturn(ResponseEntity.ok(jsonify(data)));
     ResponseEntity<DataSubmissionResponse> response = postSubmission(submissionPayloadIos, testRestTemplate,
         IOS_SERVICE_URL, false);
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.TOO_MANY_REQUESTS);
     assertThat(response.getBody().getErrorCode()).isEqualTo(PpacErrorCode.API_TOKEN_QUOTA_EXCEEDED);
-    verify(iosApiErrorHandler, times(1)).handleTooManyRequestsErrors(any(), any());
+    verify(iosDeviceApiClient, times(0)).queryDeviceData(anyString(), any());
   }
+
+  @Test
+  public void testApiTokenExpiredShouldNotTriggerAppleCall() {
+      String deviceToken = buildBase64String(this.configuration.getIos().getMinDeviceTokenLength() + 1);
+      String apiToken = buildUuid();
+      OffsetDateTime now = OffsetDateTime.now();
+      Long expirationDate = getLastDayOfMonthFor(now.minusMonths(1));
+      long timestamp = getEpochSecondFor(now);
+
+      apiTokenRepository.insert(apiToken, expirationDate, expirationDate, timestamp, timestamp);
+      PPADataRequestIOS submissionPayloadIos = buildPPADataRequestIosPayload(apiToken, deviceToken, false);
+
+      ResponseEntity<DataSubmissionResponse> response = postSubmission(submissionPayloadIos, testRestTemplate,
+          IOS_SERVICE_URL, false);
+
+      assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+      assertThat(response.getBody().getErrorCode()).isEqualTo(PpacErrorCode.API_TOKEN_EXPIRED);
+      verify(iosDeviceApiClient, times(0)).queryDeviceData(anyString(), any());
+    }
 }
