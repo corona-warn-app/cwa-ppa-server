23a938f95bc0059af4f544bf69a2f4a13eafe4da
Jeremias Rößler <jeremias.roessler@sap.com>
2021-03-24 10:52:36 +0100

    Improve logging by immediately showing the problem in the message

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/errors/InternalError.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/verification/errors/InternalError.java
@@ -3,6 +3,6 @@ package app.coronawarn.datadonation.services.ppac.ios.verification.errors;
 public class InternalError extends RuntimeException {
 
   public InternalError(final Throwable cause) {
-    super("Internal error occurred", cause);
+    super("Internal error occurred: " + cause.getMessage(), cause);
   }
 }
