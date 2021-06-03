08d762a6e48d69638fb76b681cb132721726757d
ioangut <ioan.gut@sap.com>
2021-06-03 15:01:44 +0300

    Create V9__alterMetadataTables.sql

---
+++ common/persistence/src/main/resources/db/migration/V9__alterMetadataTables.sql
@@ -0,0 +1,17 @@
+ALTER TABLE key_submission_metadata_with_client_metadata ADD submitted_with_check_ins BOOLEAN;
+
+ALTER TABLE key_submission_metadata_with_user_metadata
+  ADD submitted_after_rapid_antigen_test BOOLEAN NOT NULL DEFAULT FALSE,
+  ADD pt_days_since_most_recent_date_at_risk_level INTEGER,
+  ADD pt_hours_since_high_risk_warning INTEGER;
+
+ALTER TABLE exposure_risk_metadata
+  ADD pt_risk_level INTEGER,
+  ADD pt_risk_level_changed BOOLEAN,
+  ADD pt_most_recent_date_at_risk_level DATE,
+  ADD pt_most_recent_date_changed BOOLEAN;
+
+ALTER TABLE test_result_metadata
+  ADD pt_risk_level INTEGER,
+  ADD pt_days_since_most_recent_date_at_risk_level INTEGER,
+  ADD pt_hours_since_high_risk_warning INTEGER;
