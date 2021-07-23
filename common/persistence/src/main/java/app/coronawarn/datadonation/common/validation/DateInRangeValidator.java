ff08e58442214ec672ee4e1fa9e59f738742c539
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-07-23 15:42:18 +0200

    Date/validation (#268)
    * create new Constraint: DateInRange
    
    * use new annotation @DateInRange
    
    * Add integration test for invalid date before 1970
    
    Co-authored-by: Gut Ioan <ioan.gut@sap.com>
    Co-authored-by: ioangut <67064882+ioangut@users.noreply.github.com>

---
+++ common/persistence/src/main/java/app/coronawarn/datadonation/common/validation/DateInRangeValidator.java
@@ -0,0 +1,37 @@
+package app.coronawarn.datadonation.common.validation;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
+
+import java.time.LocalDate;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class DateInRangeValidator implements ConstraintValidator<DateInRange, LocalDate> {
+
+  protected LocalDate from = LocalDate.MIN;
+
+  protected LocalDate till = LocalDate.MAX;
+
+  @Override
+  public void initialize(final DateInRange annotation) {
+    if (!isEmpty(annotation.from())) {
+      from = LocalDate.parse(annotation.from());
+    }
+    if (!isEmpty(annotation.till())) {
+      till = LocalDate.parse(annotation.till());
+    }
+
+    assert from == null || till == null || from.isBefore(till) : "{from: " + from + "} is not before {till: " + till
+        + "}";
+  }
+
+  @Override
+  public boolean isValid(final LocalDate date, final ConstraintValidatorContext context) {
+    // null values are valid
+    if (date == null) {
+      return true;
+    }
+
+    return (from.isBefore(date) || from.isEqual(date)) && (till.isEqual(date) || till.isAfter(date));
+  }
+}
