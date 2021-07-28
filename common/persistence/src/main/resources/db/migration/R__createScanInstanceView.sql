7cb4926ffc8527c0155633616e39b32130e6af46
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-07-28 11:51:37 +0200

    moved GRANT SELECT

--- common/persistence/src/main/resources/db/migration/R__createScanInstanceView.sql
+++ common/persistence/src/main/resources/db/migration/R__createScanInstanceView.sql
@@ -1,5 +1,11 @@
-CREATE OR REPLACE VIEW scan_instance_view AS
-SELECT exposure_window_id, scan_instance.id AS scan_instance_id, typical_attenuation, minimum_attenuation, seconds_since_last_scan, submitted_at
-FROM data_donation.scan_instance INNER JOIN data_donation.exposure_window ON exposure_window_id = exposure_window.id;
+DROP VIEW IF EXISTS "scan_instance_view";
 
-GRANT SELECT ON VIEW scan_instance_view TO ppdd_analytics_user;
+CREATE OR REPLACE VIEW scan_instance_view AS
+	SELECT	exposure_window_id,
+			scan_instance.id AS scan_instance_id,
+			typical_attenuation,
+			minimum_attenuation,
+			seconds_since_last_scan,
+			submitted_at
+	FROM data_donation.scan_instance 
+		INNER JOIN data_donation.exposure_window ON exposure_window_id = exposure_window.id;
--- setup/create-analytics-user.sql
+++ setup/create-analytics-user.sql
@@ -8,6 +8,7 @@ CREATE ROLE cwa_ppdd_analytics
   IN ROLE cwa_ppdd_user;
 
 GRANT CONNECT ON DATABASE "cwa-data" TO cwa_ppdd_analytics;
+GRANT USAGE ON SCHEMA data_donation TO cwa_ppdd_analytics;
 
 GRANT SELECT ON TABLE data_donation.exposure_risk_metadata,
  data_donation.test_result_metadata,
@@ -18,7 +19,7 @@ GRANT SELECT ON TABLE data_donation.exposure_risk_metadata,
  data_donation.scan_instance,
  data_donation.exposure_window,
  data_donation.flyway_schema_history TO cwa_ppdd_analytics;
+ 
+GRANT SELECT ON VIEW data_donation.scan_instance_view TO cwa_ppdd_analytics;
 
 CREATE USER "ppdd_analytics_user" WITH INHERIT IN ROLE cwa_ppdd_analytics ENCRYPTED PASSWORD '<change me>';
-
-GRANT USAGE ON SCHEMA data_donation TO ppdd_analytics_user;
