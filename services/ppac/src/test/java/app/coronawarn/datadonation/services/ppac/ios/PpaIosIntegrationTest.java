1d11a13cdf18ac13fd844349654316573e3a239c
FelixRottler <72494737+FelixRottler@users.noreply.github.com>
2021-07-29 16:53:02 +0200

    Fix/rate limit (#274)
    * fix: add logging when apitoken is used on the same day with a difference of 23 hours. Added additional tests to cover the update step of an api token to the end of ppac
    
    * fix: organize imports
    
    * fix: tests
    
    * fix: remove code smells
    
    * fix: add validateForEdus test
    
    * Update services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/ios/verification/PpacProcessorIntegrationTest.java
    
    Co-authored-by: Hilmar Falkenberg <hilmar.falkenberg@sap.com>
    
    * fix: cover if-statement when update is 23 hours apart
    
    * fix: integration tests
    
    * fix: retrigger build
    
    * fix: integration tests
    
    * fix: integration tests
    
    Co-authored-by: Hilmar Falkenberg <hilmar.falkenberg@sap.com>

--- services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/ios/PpaIosIntegrationTest.java
+++ services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/ios/PpaIosIntegrationTest.java
@@ -6,6 +6,7 @@ import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.bu
 import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.buildUuid;
 import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.jsonify;
 import static app.coronawarn.datadonation.services.ppac.ios.testdata.TestData.postSubmission;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -14,10 +15,12 @@ import app.coronawarn.datadonation.common.config.UrlConstants;
 import app.coronawarn.datadonation.common.persistence.repository.ApiTokenRepository;
 import app.coronawarn.datadonation.common.persistence.repository.DeviceTokenRepository;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPADataRequestIOS;
+import app.coronawarn.datadonation.services.ppac.commons.web.DataSubmissionResponse;
 import app.coronawarn.datadonation.services.ppac.config.PpacConfiguration;
 import app.coronawarn.datadonation.services.ppac.ios.client.IosDeviceApiClient;
 import app.coronawarn.datadonation.services.ppac.ios.client.domain.PerDeviceDataResponse;
 import app.coronawarn.datadonation.services.ppac.ios.verification.JwtProvider;
+import feign.FeignException;
 import java.time.OffsetDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,10 +30,10 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-
 @Profile("test")
 public class PpaIosIntegrationTest {
 
@@ -53,16 +56,34 @@ public class PpaIosIntegrationTest {
 
   @BeforeEach
   void clearDatabase() {
+    deviceTokenRepository.deleteAll();
+    apiTokenRepository.deleteAll();
     when(jwtProvider.generateJwt()).thenReturn("jwt");
   }
 
   @Test
-  public void testSavePpaDataRequestIos() {
+  void testSavePpaDataRequestIos() {
     PPADataRequestIOS ppaDataRequestIOS = buildPPADataRequestIosPayload(buildUuid(),
         buildBase64String(ppacConfiguration.getIos().getMinDeviceTokenLength() + 1), true);
-    PerDeviceDataResponse data = buildIosDeviceData(OffsetDateTime.now(), true);
+    PerDeviceDataResponse data = buildIosDeviceData(OffsetDateTime.now().minusMonths(1), true);
     when(iosDeviceApiClient.queryDeviceData(anyString(), any())).thenReturn(ResponseEntity.ok(jsonify(data)));
-    postSubmission(ppaDataRequestIOS, testRestTemplate, UrlConstants.IOS + UrlConstants.DATA, false);
+    when(iosDeviceApiClient.updatePerDeviceData(anyString(), any())).thenReturn(ResponseEntity.ok().build());
+    final ResponseEntity<DataSubmissionResponse> responseEntity = postSubmission(
+        ppaDataRequestIOS, testRestTemplate, UrlConstants.IOS + UrlConstants.DATA, false);
+
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
   }
 
+  @Test
+  void shouldFailWhenUpdatingDeviceTokenFails() {
+    PPADataRequestIOS ppaDataRequestIOS = buildPPADataRequestIosPayload(buildUuid(),
+        buildBase64String(ppacConfiguration.getIos().getMinDeviceTokenLength() + 1), true);
+    PerDeviceDataResponse data = buildIosDeviceData(OffsetDateTime.now().minusMonths(1), true);
+    when(iosDeviceApiClient.queryDeviceData(anyString(), any())).thenReturn(ResponseEntity.ok(jsonify(data)));
+    when(iosDeviceApiClient.updatePerDeviceData(anyString(), any())).thenThrow(FeignException.class);
+    final ResponseEntity<DataSubmissionResponse> response = postSubmission(
+        ppaDataRequestIOS, testRestTemplate, UrlConstants.IOS + UrlConstants.DATA, false);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 }
