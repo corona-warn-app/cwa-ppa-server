9671b4b829d99bf5fd387726f2e7b4826b9cd6ef
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-08-11 11:55:06 +0200

    alter ScanInstanceRepository

--- common/persistence/src/main/java/app/coronawarn/datadonation/common/persistence/repository/metrics/ScanInstanceRepository.java
+++ common/persistence/src/main/java/app/coronawarn/datadonation/common/persistence/repository/metrics/ScanInstanceRepository.java
@@ -1,10 +1,15 @@
 package app.coronawarn.datadonation.common.persistence.repository.metrics;
 
 import app.coronawarn.datadonation.common.persistence.domain.metrics.ScanInstance;
+import java.time.LocalDate;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
-@Repository
 public interface ScanInstanceRepository extends CrudRepository<ScanInstance, Long> {
 
+  @Modifying
+  @Query("delete from scan_instance where submitted_at < :threshold")
+  void deleteOlderThan(@Param("threshold") LocalDate threshold);
 }
