27ab9c46a0ffc9f39b5da5be6db2f9ec8e86d69d
FelixRottler <fero429@gmail.com>
2021-08-13 10:45:27 +0200

    fix: ambiguous submitted_at when creating the view

--- common/persistence/src/main/resources/db/migration/R__createScanInstanceView.sql
+++ common/persistence/src/main/resources/db/migration/R__createScanInstanceView.sql
@@ -6,6 +6,6 @@ CREATE OR REPLACE VIEW scan_instance_view AS
 			typical_attenuation,
 			minimum_attenuation,
 			seconds_since_last_scan,
-			submitted_at
-	FROM data_donation.scan_instance 
+			scan_instance.submitted_at
+	FROM data_donation.scan_instance
 		INNER JOIN data_donation.exposure_window ON exposure_window_id = exposure_window.id;
