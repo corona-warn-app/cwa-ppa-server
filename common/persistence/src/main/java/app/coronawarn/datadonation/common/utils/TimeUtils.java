fdd3c51a768ff1c48a1916a5f145b3f2fa13b388
ioangut <67064882+ioangut@users.noreply.github.com>
2021-09-09 14:39:38 +0300

    Release2.9tomain (#300)
    * release 2.9
    
    * Fix that the retention deletes still valid API keys (#295)
    
    * Fix that the retention deletes still valid API keys
    
    * Change the way the test data is generated, to make it match the expiration criteria
    
    * Increase the amount of data generated
    
    * Fix/time utils error (#297)
    
    * Statically import UTC
    
    * Hopefully the least complex solution
    
    * Make clock final
    
    * Doesn't need to be static, so @BeforeEach
    
    * Use @Autowired to ensure that the correct constructor is used
    
    * Decrease visibility to show that it's NOT the standard constructor AND add corresponding logging
    
    * Update maven.config
    
    Co-authored-by: Jeremias Rößler <jeremias.roessler@sap.com>

--- common/persistence/src/main/java/app/coronawarn/datadonation/common/utils/TimeUtils.java
+++ common/persistence/src/main/java/app/coronawarn/datadonation/common/utils/TimeUtils.java
@@ -1,11 +1,11 @@
 package app.coronawarn.datadonation.common.utils;
 
+import static java.time.ZoneOffset.UTC;
+
 import java.time.DateTimeException;
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.temporal.TemporalAdjusters;
 
@@ -13,8 +13,6 @@ import java.time.temporal.TemporalAdjusters;
  * Time related business logic. All times are handled in UTC time
  */
 public class TimeUtils {
-
-  private static Instant now;
 
   private TimeUtils() {
   }
@@ -26,7 +24,7 @@ public class TimeUtils {
    * @return the epoch seconds for the last day in the provided month
    */
   public static Long getLastDayOfMonthFor(OffsetDateTime offsetDateTime) {
-    return offsetDateTime.withOffsetSameInstant(ZoneOffset.UTC).with(TemporalAdjusters.lastDayOfMonth())
+    return offsetDateTime.withOffsetSameInstant(UTC).with(TemporalAdjusters.lastDayOfMonth())
         .toEpochSecond();
   }
 
@@ -36,7 +34,7 @@ public class TimeUtils {
    * @return the epoch seconds of the current month in UTC.
    */
   public static Long getLastDayOfMonthForNow() {
-    return OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC).with(TemporalAdjusters.lastDayOfMonth())
+    return OffsetDateTime.now().withOffsetSameInstant(UTC).with(TemporalAdjusters.lastDayOfMonth())
         .toEpochSecond();
   }
 
@@ -47,9 +45,9 @@ public class TimeUtils {
    * @return the epoch seconds of the provided time in UTC.
    */
   public static Long getEpochSecondFor(OffsetDateTime time) {
-    return time.withOffsetSameInstant(ZoneOffset.UTC).toEpochSecond();
+    return time.withOffsetSameInstant(UTC).toEpochSecond();
   }
-  
+
   /**
    * Calculate the LocalDate based on epoch seconds in UTC.
    *
@@ -58,7 +56,7 @@ public class TimeUtils {
    * @throws DateTimeException if epochSecond > {@link Instant#MAX} or epochSecond < {@link Instant#MIN}
    */
   public static LocalDate getLocalDateFor(Long epochSecond) {
-    return Instant.ofEpochSecond(epochSecond).atOffset(ZoneOffset.UTC).toLocalDate();
+    return Instant.ofEpochSecond(epochSecond).atOffset(UTC).toLocalDate();
   }
 
   /**
@@ -68,7 +66,7 @@ public class TimeUtils {
    * @return a ZonedDateTime representing the provided epoch seconds.
    */
   public static ZonedDateTime getZonedDateTimeFor(Long epochSeconds) {
-    return Instant.ofEpochSecond(epochSeconds).atOffset(ZoneOffset.UTC).toZonedDateTime();
+    return Instant.ofEpochSecond(epochSeconds).atOffset(UTC).toZonedDateTime();
   }
 
   /**
@@ -86,16 +84,7 @@ public class TimeUtils {
    * @return the parsed LocalDate.
    */
   public static LocalDate getLocalDateForNow() {
-    return getNow().atOffset(ZoneOffset.UTC).toLocalDate();
-  }
-
-  /**
-   * Calculate the LocalDateTime of the current Timestamp in UTC.
-   *
-   * @return the parsed LocalDateTime.
-   */
-  public static LocalDateTime getLocalDateTimeForNow() {
-    return getNow().atOffset(ZoneOffset.UTC).toLocalDateTime();
+    return Instant.now().atOffset(UTC).toLocalDate();
   }
 
   /**
@@ -104,7 +93,7 @@ public class TimeUtils {
    * @return the epoch milli seconds of the current Timestamp.
    */
   public static Long getEpochMilliSecondForNow() {
-    return getNow().toEpochMilli();
+    return Instant.now().toEpochMilli();
   }
 
   /**
@@ -113,27 +102,6 @@ public class TimeUtils {
    * @return the epoch seconds of the current Timestamp.
    */
   public static Long getEpochSecondsForNow() {
-    return getNow().getEpochSecond();
-  }
-
-  /**
-   * Returns the UTC {@link Instant} time or creates a new instance if called the first time.
-   *
-   * @return current Instant
-   */
-  public static Instant getNow() {
-    if (now == null) {
-      now = Instant.now();
-    }
-    return now;
-  }
-
-  /**
-   * Injects UTC instant time value.
-   *
-   * @param instant an {@link Instant} object.
-   */
-  public static void setNow(Instant instant) {
-    now = instant;
+    return Instant.now().getEpochSecond();
   }
 }
