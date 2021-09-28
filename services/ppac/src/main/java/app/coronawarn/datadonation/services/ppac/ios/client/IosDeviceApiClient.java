7682c08cdbd8884305ef3ca68c9ca820ed8a2445
Gut Ioan <ioan.gut@sap.com>
2021-09-28 11:24:19 +0300

    Using AOP initial commit

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/client/IosDeviceApiClient.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ios/client/IosDeviceApiClient.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "deviceApi", url = "${ppac.ios.device-api-url}")
+@Loggable
 public interface IosDeviceApiClient {
 
   @PostMapping(value = "/query_two_bits")
