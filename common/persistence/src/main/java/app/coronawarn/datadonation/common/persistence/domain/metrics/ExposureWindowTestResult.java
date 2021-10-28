fcf5522ce2fa3c13a3a0beb16e6f04ce243631cf
Gut Ioan <ioan.gut@sap.com>
2021-10-28 16:34:15 +0300

    add hashCode and equals methods for the models

--- common/persistence/src/main/java/app/coronawarn/datadonation/common/persistence/domain/metrics/ExposureWindowTestResult.java
+++ common/persistence/src/main/java/app/coronawarn/datadonation/common/persistence/domain/metrics/ExposureWindowTestResult.java
@@ -1,6 +1,7 @@
 package app.coronawarn.datadonation.common.persistence.domain.metrics;
 
 import app.coronawarn.datadonation.common.persistence.domain.metrics.embeddable.ClientMetadataDetails;
+import java.util.Objects;
 import java.util.Set;
 import javax.validation.constraints.NotNull;
 import org.springframework.data.relational.core.mapping.Embedded;
@@ -37,16 +38,52 @@ public class ExposureWindowTestResult extends DataDonationMetric {
     return testResult;
   }
 
-  public ClientMetadataDetails getClientMetadata() {
-    return clientMetadata;
-  }
-
-  public TechnicalMetadata getTechnicalMetadata() {
-    return technicalMetadata;
-  }
-
   public Set<ExposureWindowsAtTestRegistration> getExposureWindowsAtTestRegistrations() {
     return exposureWindowsAtTestRegistrations;
   }
 
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    ExposureWindowTestResult other = (ExposureWindowTestResult) obj;
+    if (technicalMetadata == null) {
+      if (other.technicalMetadata != null) {
+        return false;
+      }
+    } else if (!technicalMetadata.equals(other.technicalMetadata)) {
+      return false;
+    }
+    if (testResult == null) {
+      if (other.testResult != null) {
+        return false;
+      }
+    } else if (!testResult.equals(other.testResult)) {
+      return false;
+    }
+    if (clientMetadata == null) {
+      if (other.clientMetadata != null) {
+        return false;
+      }
+    } else if (!clientMetadata.equals(other.clientMetadata)) {
+      return false;
+    }
+    if (exposureWindowsAtTestRegistrations == null) {
+      if (other.exposureWindowsAtTestRegistrations != null) {
+        return false;
+      }
+    } else if (!exposureWindowsAtTestRegistrations.equals(other.exposureWindowsAtTestRegistrations)) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, testResult, clientMetadata, technicalMetadata, exposureWindowsAtTestRegistrations);
+  }
 }
--- common/persistence/src/main/java/app/coronawarn/datadonation/common/persistence/domain/metrics/ExposureWindowsAtTestRegistration.java
+++ common/persistence/src/main/java/app/coronawarn/datadonation/common/persistence/domain/metrics/ExposureWindowsAtTestRegistration.java
@@ -1,6 +1,7 @@
 package app.coronawarn.datadonation.common.persistence.domain.metrics;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Set;
 import javax.validation.constraints.NotNull;
 import org.springframework.data.relational.core.mapping.MappedCollection;
@@ -41,5 +42,79 @@ public class ExposureWindowsAtTestRegistration extends DataDonationMetric {
     this.transmissionRiskLevel = transmissionRiskLevel;
     this.normalizedTime = normalizedTime;
     this.scanInstancesAtTestRegistration = scanInstancesAtTestRegistration;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    ExposureWindowsAtTestRegistration other = (ExposureWindowsAtTestRegistration) obj;
+    if (exposureWindowTestResultId == null) {
+      if (other.exposureWindowTestResultId != null) {
+        return false;
+      }
+    } else if (!exposureWindowTestResultId.equals(other.exposureWindowTestResultId)) {
+      return false;
+    }
+    if (date == null) {
+      if (other.date != null) {
+        return false;
+      }
+    } else if (!date.equals(other.date)) {
+      return false;
+    }
+    if (reportType == null) {
+      if (other.reportType != null) {
+        return false;
+      }
+    } else if (!reportType.equals(other.reportType)) {
+      return false;
+    }
+    if (infectiousness == null) {
+      if (other.infectiousness != null) {
+        return false;
+      }
+    } else if (!infectiousness.equals(other.infectiousness)) {
+      return false;
+    }
+    if (calibrationConfidence == null) {
+      if (other.calibrationConfidence != null) {
+        return false;
+      }
+    } else if (!calibrationConfidence.equals(other.calibrationConfidence)) {
+      return false;
+    }
+    if (transmissionRiskLevel == null) {
+      if (other.transmissionRiskLevel != null) {
+        return false;
+      }
+    } else if (!transmissionRiskLevel.equals(other.transmissionRiskLevel)) {
+      return false;
+    }
+    if (normalizedTime == null) {
+      if (other.normalizedTime != null) {
+        return false;
+      }
+    } else if (!normalizedTime.equals(other.normalizedTime)) {
+      return false;
+    }
+    if (scanInstancesAtTestRegistration == null) {
+      if (other.scanInstancesAtTestRegistration != null) {
+        return false;
+      }
+    } else if (!scanInstancesAtTestRegistration.equals(other.scanInstancesAtTestRegistration)) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, exposureWindowTestResultId, date, reportType, infectiousness, calibrationConfidence,
+        transmissionRiskLevel, normalizedTime, scanInstancesAtTestRegistration);
   }
 }
