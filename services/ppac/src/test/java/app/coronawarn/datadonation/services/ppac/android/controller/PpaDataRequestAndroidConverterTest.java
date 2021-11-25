2143a8ab606cf61c3cf338a6410d9db36e3f6bab
ioangut <67064882+ioangut@users.noreply.github.com>
2021-11-25 15:53:08 +0200

    Ppa submission tests enhancement (#342)
    * Add converter tests for supported/unsupported PPATestResult
    
    * Add integration checks for db models created for IosSubmission
    
    * Add documentation for env variable
    
    * Update services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/android/controller/PpaDataRequestAndroidConverterTest.java

--- services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/android/controller/PpaDataRequestAndroidConverterTest.java
+++ services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/android/controller/PpaDataRequestAndroidConverterTest.java
@@ -1,26 +1,63 @@
 package app.coronawarn.datadonation.services.ppac.android.controller;
 
+import static app.coronawarn.datadonation.common.protocols.internal.ppdd.PPARiskLevel.RISK_LEVEL_HIGH;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import app.coronawarn.datadonation.common.persistence.domain.metrics.ExposureWindowTestResult;
 import app.coronawarn.datadonation.common.persistence.domain.metrics.TechnicalMetadata;
 import app.coronawarn.datadonation.common.persistence.domain.metrics.embeddable.ClientMetadataDetails;
 import app.coronawarn.datadonation.common.persistence.domain.metrics.embeddable.CwaVersionMetadata;
+import app.coronawarn.datadonation.common.persistence.service.PpaDataStorageRequest;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.ExposureRiskMetadata;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPAClientMetadataAndroid;
+import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPADataAndroid;
+import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPADataIOS;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPADataRequestAndroid;
+import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPADataRequestIOS;
+import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPAExposureWindow;
+import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPAExposureWindowInfectiousness;
+import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPANewExposureWindow;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPARiskLevel;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPASemanticVersion;
+import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPATestResult;
+import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPATestResultMetadata;
 import app.coronawarn.datadonation.common.protocols.internal.ppdd.PPAUserMetadata;
-import app.coronawarn.datadonation.services.ppac.commons.PpaDataRequestConverter;
+import app.coronawarn.datadonation.common.utils.TimeUtils;
+import app.coronawarn.datadonation.services.ppac.android.attestation.AttestationStatement;
+import app.coronawarn.datadonation.services.ppac.config.PpacConfiguration;
 import java.time.LocalDate;
 import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@ExtendWith(MockitoExtension.class)
 public class PpaDataRequestAndroidConverterTest
-    extends PpaDataRequestConverter<PPADataRequestAndroid, PPAClientMetadataAndroid> {
+    extends PpaDataRequestAndroidConverter {
+
+  private PpacConfiguration ppacConfig;
+
+  @InjectMocks
+  private AttestationStatement attestationStatement;
+
+  @BeforeEach
+  public void setup() {
+    ppacConfig = new PpacConfiguration();
+    //The maximum number of exposure windows to store per submission.
+    //672 = 24 hours per day * 0,5 hours per Exposure Window * 14 days
+    ppacConfig.setMaxExposureWindowsToRejectSubmission(672);
+    ppacConfig.setMaxExposureWindowsToStore(672);
+  }
 
   @Test
   public void convertToExposureMetricsTestRiskLevelUnknownValue() {
@@ -61,6 +98,91 @@ public class PpaDataRequestAndroidConverterTest
     assertTrue(dbExposureRiskMetadata.getPtRiskLevelChanged());
     assertEquals(LocalDate.of(1970, 1, 1), dbExposureRiskMetadata.getPtMostRecentDateAtRiskLevel());
     assertFalse(dbExposureRiskMetadata.getPtMostRecentDateChanged());
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = PPATestResult.class,
+      names = {"TEST_RESULT_POSITIVE", "TEST_RESULT_NEGATIVE", "TEST_RESULT_RAT_POSITIVE", "TEST_RESULT_RAT_NEGATIVE"})
+  public void testConvertToExposureWindowTestResults(PPATestResult ppaTestResults) {
+    final Long epochSecondForNow = TimeUtils.getEpochSecondsForNow();
+    final PPAExposureWindow ppaExposureWindow = PPAExposureWindow
+        .newBuilder()
+        .setCalibrationConfidence(1)
+        .setInfectiousness(PPAExposureWindowInfectiousness.INFECTIOUSNESS_HIGH)
+        .setDate(epochSecondForNow)
+        .build();
+
+    final PPANewExposureWindow ppaNewExposureWindow = PPANewExposureWindow
+        .newBuilder()
+        .setExposureWindow(ppaExposureWindow)
+        .build();
+
+    final PPATestResultMetadata ppaTestResultMetadata =
+        PPATestResultMetadata.newBuilder()
+            .setTestResult(ppaTestResults)
+            .setRiskLevelAtTestRegistration(RISK_LEVEL_HIGH)
+            .setHoursSinceTestRegistration(5)
+            .setHoursSinceHighRiskWarningAtTestRegistration(5)
+            .setDaysSinceMostRecentDateAtRiskLevelAtTestRegistration(5)
+            .addExposureWindowsAtTestRegistration(ppaNewExposureWindow)
+            .addExposureWindowsUntilTestResult(ppaNewExposureWindow)
+            .build();
+
+    final PPADataAndroid payload = PPADataAndroid.newBuilder()
+        .addTestResultMetadataSet(ppaTestResultMetadata).build();
+
+    PPADataRequestAndroid ppaDataRequestAndroid = PPADataRequestAndroid.newBuilder()
+        .setPayload(payload).build();
+    // when
+    final PpaDataStorageRequest ppaDataStorageRequest = convertToStorageRequest(ppaDataRequestAndroid, ppacConfig,
+        attestationStatement);
+    assertThat(ppaDataStorageRequest).isNotNull();
+    assertThat(ppaDataStorageRequest.getTestResultMetric()).isPresent();
+    final List<ExposureWindowTestResult> testResultsMetadata = ppaDataStorageRequest
+        .getExposureWindowTestResult().get();
+    assertThat(testResultsMetadata.get(0).getTestResult()).isEqualTo(ppaTestResults.getNumber());
+    assertThat(testResultsMetadata.get(0).getExposureWindowsAtTestRegistrations().size()).isEqualTo(2);
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = PPATestResult.class,
+      names = {"TEST_RESULT_RAT_PENDING", "TEST_RESULT_UNKNOWN", "TEST_RESULT_PENDING", "TEST_RESULT_RAT_INVALID"})
+  public void testConvertToExposureWindowTestResultsFailedBecausePpaTestResult(PPATestResult ppaTestResults) {
+    final Long epochSecondForNow = TimeUtils.getEpochSecondsForNow();
+    final PPAExposureWindow ppaExposureWindow = PPAExposureWindow
+        .newBuilder()
+        .setCalibrationConfidence(1)
+        .setInfectiousness(PPAExposureWindowInfectiousness.INFECTIOUSNESS_HIGH)
+        .setDate(epochSecondForNow)
+        .build();
+
+    final PPANewExposureWindow ppaNewExposureWindow = PPANewExposureWindow
+        .newBuilder()
+        .setExposureWindow(ppaExposureWindow)
+        .build();
+
+    final PPATestResultMetadata ppaTestResultMetadata =
+        PPATestResultMetadata.newBuilder()
+            .setTestResult(ppaTestResults)
+            .setRiskLevelAtTestRegistration(RISK_LEVEL_HIGH)
+            .setHoursSinceTestRegistration(5)
+            .setHoursSinceHighRiskWarningAtTestRegistration(5)
+            .setDaysSinceMostRecentDateAtRiskLevelAtTestRegistration(5)
+            .addExposureWindowsAtTestRegistration(ppaNewExposureWindow)
+            .addExposureWindowsUntilTestResult(ppaNewExposureWindow)
+            .build();
+
+    final PPADataAndroid payload = PPADataAndroid.newBuilder()
+        .addTestResultMetadataSet(ppaTestResultMetadata).build();
+
+    PPADataRequestAndroid ppaDataRequestIOS = PPADataRequestAndroid.newBuilder()
+        .setPayload(payload).build();
+    // when
+    final PpaDataStorageRequest ppaDataStorageRequest = convertToStorageRequest(ppaDataRequestIOS,
+        ppacConfig, attestationStatement);
+    assertThat(ppaDataStorageRequest).isNotNull();
+    assertThat(ppaDataStorageRequest.getTestResultMetric()).isPresent();
+    assertThat(ppaDataStorageRequest.getExposureWindowTestResult()).contains(Collections.emptyList());
   }
 
   @Override
