b0d75c608b86d7169a14cd2b92671f31dac6838e
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2021-12-20 10:47:03 +0100

    shutdownHook="disable", log4j dependeny obsolete
    same as https://github.com/corona-warn-app/cwa-server/pull/1663

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ServerApplication.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/ServerApplication.java
@@ -3,10 +3,8 @@ package app.coronawarn.datadonation.services.ppac;
 import app.coronawarn.datadonation.services.ppac.config.PpacConfiguration;
 import java.util.Arrays;
 import java.util.List;
-import org.apache.logging.log4j.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -27,20 +25,12 @@ import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
     "app.coronawarn.datadonation.services.ppac"})
 @EnableConfigurationProperties(PpacConfiguration.class)
 @EnableFeignClients
-public class ServerApplication implements EnvironmentAware, DisposableBean {
+public class ServerApplication implements EnvironmentAware {
 
   private static final Logger logger = LoggerFactory.getLogger(ServerApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(ServerApplication.class);
-  }
-
-  /**
-   * Manual shutdown hook needed to avoid Log4j shutdown issues (see cwa-server/#589).
-   */
-  @Override
-  public void destroy() {
-    LogManager.shutdown();
   }
 
   @Bean
