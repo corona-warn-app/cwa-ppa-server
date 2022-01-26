4a2e70086556c84e14de5f59bc62c705271dd6f7
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-01-26 16:01:19 +0100

    alter SEQUENCEs, primary keys and their fks

--- common/persistence/src/main/resources/db/migration/V20__alterFKtoBigInt.sql
+++ common/persistence/src/main/resources/db/migration/V20__alterFKtoBigInt.sql
@@ -1,8 +1,13 @@
-ALTER TABLE scan_instance
-  ALTER COLUMN exposure_window_id TYPE bigint;
+ALTER TABLE scan_instance ALTER COLUMN exposure_window_id TYPE BIGINT;
 
-ALTER TABLE exposure_windows_at_test_registration
-  ALTER COLUMN exposure_window_test_result_id TYPE bigint;
+ALTER TABLE    exposure_windows_at_test_registration ALTER COLUMN id TYPE BIGINT, 
+                                                     ALTER COLUMN exposure_window_test_result_id TYPE BIGINT;
+ALTER SEQUENCE exposure_windows_at_test_registration_id_seq AS BIGINT;
 
-ALTER TABLE scan_instances_at_test_registration
-  ALTER COLUMN exposure_window_id TYPE bigint;
+ALTER TABLE scan_instances_at_test_registration ALTER COLUMN exposure_window_id TYPE BIGINT;
+
+ALTER TABLE    exposure_window_test_result ALTER COLUMN id TYPE BIGINT;
+ALTER SEQUENCE exposure_window_test_result_id_seq AS BIGINT;
+
+ALTER TABLE    summarized_exposure_windows_with_user_metadata ALTER COLUMN id TYPE BIGINT;
+ALTER SEQUENCE summarized_exposure_windows_with_user_metadata_id_seq AS BIGINT;
