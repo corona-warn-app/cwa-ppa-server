e31161e9f56840b346d12c1f855e0f6e92f8f13a
Gut Ioan <ioan.gut@sap.com>
2022-03-11 16:23:32 +0200

    Create SaltServiceTest.java

---
+++ common/persistence/src/test/java/app/coronawarn/datadonation/common/persistence/service/SaltServiceTest.java
@@ -0,0 +1,28 @@
+package app.coronawarn.datadonation.common.persistence.service;
+
+import app.coronawarn.datadonation.common.persistence.repository.ppac.android.SaltRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+
+@SpringBootTest
+@DirtiesContext
+class SaltServiceTest {
+
+  @MockBean
+  SaltRepository saltRepository;
+
+  @Test
+  void testThrownRuntimeExceptionForSaltRepository() {
+    doThrow(new RuntimeException()).when(saltRepository).deleteSalt(any());
+
+    assertThrows(RuntimeException.class,
+        () -> saltRepository.deleteSalt("test"));
+  }
+}
