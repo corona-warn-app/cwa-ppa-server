f19974e2286e7bd08bda9fb5a58aeac7f0a1252d
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-03-17 11:19:56 +0100

    don't log user input

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/android/attestation/salt/ProdSaltVerificationStrategy.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/android/attestation/salt/ProdSaltVerificationStrategy.java
@@ -7,8 +7,6 @@ import app.coronawarn.datadonation.services.ppac.android.attestation.errors.Salt
 import app.coronawarn.datadonation.services.ppac.config.PpacConfiguration;
 import java.time.Instant;
 import java.util.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
@@ -16,8 +14,6 @@ import org.springframework.util.ObjectUtils;
 @Component
 @Profile("!loadtest")
 public class ProdSaltVerificationStrategy implements SaltVerificationStrategy {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(ProdSaltVerificationStrategy.class);
 
   private final SaltRepository saltRepository;
   private final PpacConfiguration appParameters;
@@ -35,7 +31,6 @@ public class ProdSaltVerificationStrategy implements SaltVerificationStrategy {
    * Verify that the given salt has not been redeemed (expired).
    */
   public void validateSalt(String saltString) {
-    LOGGER.debug("Salt received: {}", saltString);
     if (ObjectUtils.isEmpty(saltString)) {
       throw new MissingMandatoryAuthenticationFields("No salt received");
     }
