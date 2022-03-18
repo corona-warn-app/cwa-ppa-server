157e9ac4315c97a2515b15ccdd64d3785a2862e6
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-03-18 09:22:50 +0100

    Dependency/clean up (#443)
    * guava can easily be replaces with spring utils
    
    * dom4j not used
    
    * don't log user input
    
    * cleaned 'persistence' module
    
    * minify

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/android/attestation/salt/ProdSaltVerificationStrategy.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/android/attestation/salt/ProdSaltVerificationStrategy.java
@@ -5,19 +5,15 @@ import app.coronawarn.datadonation.common.persistence.repository.ppac.android.Sa
 import app.coronawarn.datadonation.services.ppac.android.attestation.errors.MissingMandatoryAuthenticationFields;
 import app.coronawarn.datadonation.services.ppac.android.attestation.errors.SaltNotValidAnymore;
 import app.coronawarn.datadonation.services.ppac.config.PpacConfiguration;
-import com.google.common.base.Strings;
 import java.time.Instant;
 import java.util.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 @Component
 @Profile("!loadtest")
 public class ProdSaltVerificationStrategy implements SaltVerificationStrategy {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(ProdSaltVerificationStrategy.class);
 
   private final SaltRepository saltRepository;
   private final PpacConfiguration appParameters;
@@ -35,8 +31,7 @@ public class ProdSaltVerificationStrategy implements SaltVerificationStrategy {
    * Verify that the given salt has not been redeemed (expired).
    */
   public void validateSalt(String saltString) {
-    LOGGER.debug("Salt received: {}", saltString);
-    if (Strings.isNullOrEmpty(saltString)) {
+    if (ObjectUtils.isEmpty(saltString)) {
       throw new MissingMandatoryAuthenticationFields("No salt received");
     }
     Optional<SaltData> saltOptional = saltRepository.findById(saltString);
