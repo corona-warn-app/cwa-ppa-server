1f5b12b82b1eca39da2f644283ccc2bb08278ec1
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-11-22 08:43:07 +0100

    test execution around second 59 is not predictable

--- services/els-verify/src/test/java/app/coronawarn/datadonation/services/els/otp/GenerateElsOtpControllerTest.java
+++ services/els-verify/src/test/java/app/coronawarn/datadonation/services/els/otp/GenerateElsOtpControllerTest.java
@@ -5,8 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import app.coronawarn.datadonation.common.persistence.service.OtpTestGenerationResponse;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.util.List;
+import java.util.Collection;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -17,24 +19,31 @@ import org.springframework.test.context.ActiveProfiles;
 @DirtiesContext
 class GenerateElsOtpControllerTest {
 
+  private static final Logger logger = LoggerFactory.getLogger(GenerateElsOtpControllerTest.class);
+
   @Autowired
   GenerateElsOtpController generateElsOtpController;
 
   @Test
   void testElsOtpsAreCreated() {
-    int numberOfInvocations = 15;
-    int validityInHours = 5;
+    final int numberOfInvocations = 15;
+    final int validityInHours = 5;
 
-    List<OtpTestGenerationResponse> responses = generateElsOtpController
-        .generateElsOtp(numberOfInvocations, validityInHours)
-        .getBody();
+    final Collection<OtpTestGenerationResponse> responses = generateElsOtpController
+        .generateElsOtp(numberOfInvocations, validityInHours).getBody();
+    final ZonedDateTime expected = ZonedDateTime.now(ZoneOffset.UTC).plusHours(validityInHours);
 
     assert responses != null;
     assertThat(responses.size()).isEqualTo(numberOfInvocations);
 
-    for (OtpTestGenerationResponse response : responses) {
-      assertThat(ZonedDateTime.now(ZoneOffset.UTC).plusHours(validityInHours))
-          .isEqualToIgnoringSeconds(response.getExpirationDate());
+    if (expected.getSecond() < 1) {
+      // chance is high, that the test was started at second 59, but execution time was too long and now we're in a
+      // different minute, so let's skip the assertions!
+      logger.warn("skipping: {} - 'testElsOtpsAreCreated'", this);
+      return;
+    }
+    for (final OtpTestGenerationResponse response : responses) {
+      assertThat(expected).isEqualToIgnoringSeconds(response.getExpirationDate());
     }
   }
 }
