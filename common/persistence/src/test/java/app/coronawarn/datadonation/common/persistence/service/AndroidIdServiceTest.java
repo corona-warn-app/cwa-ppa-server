ab795b6b3a50131a525b89e6096a058f18e1b67f
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-11-29 14:26:43 +0100

    create pepperGenerator on the fly

--- common/persistence/src/test/java/app/coronawarn/datadonation/common/persistence/service/AndroidIdServiceTest.java
+++ common/persistence/src/test/java/app/coronawarn/datadonation/common/persistence/service/AndroidIdServiceTest.java
@@ -3,10 +3,25 @@ package app.coronawarn.datadonation.common.persistence.service;
 import static app.coronawarn.datadonation.common.persistence.service.AndroidIdService.pepper;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.HexFormat;
 import org.junit.jupiter.api.Test;
 
 class AndroidIdServiceTest {
+
+  /**
+   * Prints newly generated 'Pepper' which can be used as secret in Vault.
+   *
+   * @param args
+   * @throws NoSuchAlgorithmException
+   */
+  public static void main(final String[] args) throws NoSuchAlgorithmException {
+    final byte[] b16 = new byte[16];
+    SecureRandom.getInstanceStrong().nextBytes(b16);
+    final String pepper = HexFormat.of().formatHex(b16);
+    System.out.println(pepper);
+  }
 
   @Test
   final void testPepperByteArrayByteArray() {
@@ -14,5 +29,20 @@ class AndroidIdServiceTest {
     final String pepper = "7c92700f863098670f865d3151299543";
     assertEquals("P0eD7H3CP5/arItLUvEpXw5nYOR+SCBznLPl5xYscWY=",
         pepper(HexFormat.of().parseHex(androidId), HexFormat.of().parseHex(pepper)));
+  }
+
+  @Test
+  final void testPepperByteArrayByteArrayRandom() throws NoSuchAlgorithmException {
+    final byte[] b8 = new byte[8];
+    SecureRandom.getInstanceStrong().nextBytes(b8);
+    final String androidId = HexFormat.of().formatHex(b8);
+
+    final byte[] b16 = new byte[16];
+    SecureRandom.getInstanceStrong().nextBytes(b16);
+    final String pepper = HexFormat.of().formatHex(b16);
+
+    final String pepperedId = pepper(HexFormat.of().parseHex(androidId), HexFormat.of().parseHex(pepper));
+    assertEquals(44, pepperedId.length());
+    assertEquals('=', pepperedId.charAt(43));
   }
 }
