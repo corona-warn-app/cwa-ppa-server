e974e5c8671964a8a3c9fbff5ca621bfff1caffe
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-11-30 11:22:15 +0100

    test with random AndroidId

--- services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/android/attestation/ProdAndroidIdVerificationStrategyTest.java
+++ services/ppac/src/test/java/app/coronawarn/datadonation/services/ppac/android/attestation/ProdAndroidIdVerificationStrategyTest.java
@@ -1,39 +1,43 @@
 package app.coronawarn.datadonation.services.ppac.android.attestation;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import app.coronawarn.datadonation.services.ppac.android.attestation.errors.AndroidIdNotValid;
 import app.coronawarn.datadonation.services.ppac.config.PpacConfiguration;
+import java.security.SecureRandom;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class ProdAndroidIdVerificationStrategyTest {
 
-    ProdAndroidIdVerificationStrategy idVerificationStrategy;
+  ProdAndroidIdVerificationStrategy idVerificationStrategy;
 
-    @BeforeEach
-    public void setup() {
-        PpacConfiguration ppacConfiguration = new PpacConfiguration();
-        PpacConfiguration.Android androidParameters = new PpacConfiguration.Android();
-        androidParameters.setSrs(new PpacConfiguration.Android.Srs());
-        ppacConfiguration.setAndroid(androidParameters);
-        ppacConfiguration.getAndroid().getSrs().setRequireAndroidIdSyntaxCheck(true);
-        this.idVerificationStrategy = new ProdAndroidIdVerificationStrategy(ppacConfiguration);
-    }
+  @BeforeEach
+  public void setup() {
+    final PpacConfiguration ppacConfiguration = new PpacConfiguration();
+    final PpacConfiguration.Android androidParameters = new PpacConfiguration.Android();
+    androidParameters.setSrs(new PpacConfiguration.Android.Srs());
+    ppacConfiguration.setAndroid(androidParameters);
+    ppacConfiguration.getAndroid().getSrs().setRequireAndroidIdSyntaxCheck(true);
+    idVerificationStrategy = new ProdAndroidIdVerificationStrategy(ppacConfiguration);
+  }
 
-    @Test
-    void testValidateAndroidIdShouldFailDueToInvalidId() {
-        byte[] testId = new byte[7];
-        AndroidIdNotValid exception = assertThrows(AndroidIdNotValid.class, () ->
-                idVerificationStrategy.validateAndroidId(testId));
-        assertThat(exception.getMessage(), is(not(emptyOrNullString())));
-    }
+  @Test
+  void testValidateAndroidIdShouldFailDueToInvalidId() {
+    final byte[] testId = new byte[7];
+    final AndroidIdNotValid exception = assertThrows(AndroidIdNotValid.class,
+        () -> idVerificationStrategy.validateAndroidId(testId));
+    assertThat(exception.getMessage(), is(not(emptyOrNullString())));
+  }
 
-    @Test
-    void testValidateAndroidIdShouldPass() {
-        byte[] testId = new byte[8];
-        idVerificationStrategy.validateAndroidId(testId);
-    }
+  @Test
+  void testValidateAndroidIdShouldPass() throws Exception {
+    final byte[] testId = new byte[8];
+    SecureRandom.getInstanceStrong().nextBytes(testId);
+    idVerificationStrategy.validateAndroidId(testId);
+  }
 }
