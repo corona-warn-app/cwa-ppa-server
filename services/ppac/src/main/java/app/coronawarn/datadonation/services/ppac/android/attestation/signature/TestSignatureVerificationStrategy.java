55ce8e7324f8a9249d4027529cfcd9035189b8ec
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-12-14 18:29:00 +0100

    methods for tests can just throw their exceptions as they want

--- services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/android/attestation/signature/TestSignatureVerificationStrategy.java
+++ services/ppac/src/main/java/app/coronawarn/datadonation/services/ppac/android/attestation/signature/TestSignatureVerificationStrategy.java
@@ -1,5 +1,7 @@
 package app.coronawarn.datadonation.services.ppac.android.attestation.signature;
 
+import static app.coronawarn.datadonation.services.ppac.android.attestation.signature.JwsGenerationUtil.getTestCertificate;
+
 import com.google.api.client.json.webtoken.JsonWebSignature;
 import java.security.GeneralSecurityException;
 import java.security.cert.X509Certificate;
@@ -10,17 +12,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestSignatureVerificationStrategy implements SignatureVerificationStrategy {
 
-  private X509Certificate certificate = JwsGenerationUtil.getTestCertificate();
+  private final X509Certificate certificate;
 
-  public TestSignatureVerificationStrategy() {
+  public TestSignatureVerificationStrategy() throws Exception {
     // Default constructor is used in tests.
+    certificate = getTestCertificate();
   }
 
   /**
    * Just return the configured test related certificate as if it were trusted by the platform's TrustManagers.
    */
   @Override
-  public X509Certificate verifySignature(JsonWebSignature jws) throws GeneralSecurityException {
-    return this.certificate;
+  public X509Certificate verifySignature(final JsonWebSignature jws) throws GeneralSecurityException {
+    return certificate;
   }
 }
