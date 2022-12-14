d5c8154f9adb6ae6a3cb5d05c771ce9bbbaeae07
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2022-12-14 18:29:16 +0100

    set default user names

--- setup/create-users.sql
+++ setup/create-users.sql
@@ -1,6 +1,6 @@
-CREATE USER "<change me>" WITH INHERIT IN ROLE cwa_ppdd_flyway PASSWORD '<change me>';
-CREATE USER "<change me>" WITH INHERIT IN ROLE cwa_ppdd_ppac PASSWORD '<change me>';
-CREATE USER "<change me>" WITH INHERIT IN ROLE cwa_ppdd_edus PASSWORD '<change me>';
-CREATE USER "<change me>" WITH INHERIT IN ROLE cwa_ppdd_retention PASSWORD '<change me>';
-CREATE USER "<change me>" WITH INHERIT IN ROLE cwa_ppdd_els_verify PASSWORD '<change me>';
-CREATE USER "<change me>" WITH INHERIT IN ROLE cwa_ppdd_srs_verify PASSWORD '<change me>';
+CREATE USER "flyway"     WITH INHERIT IN ROLE cwa_ppdd_flyway     PASSWORD '<change me>';
+CREATE USER "ppac"       WITH INHERIT IN ROLE cwa_ppdd_ppac       PASSWORD '<change me>';
+CREATE USER "edus"       WITH INHERIT IN ROLE cwa_ppdd_edus       PASSWORD '<change me>';
+CREATE USER "retention"  WITH INHERIT IN ROLE cwa_ppdd_retention  PASSWORD '<change me>';
+CREATE USER "els_verify" WITH INHERIT IN ROLE cwa_ppdd_els_verify PASSWORD '<change me>';
+CREATE USER "srs_verify" WITH INHERIT IN ROLE cwa_ppdd_srs_verify PASSWORD '<change me>';
