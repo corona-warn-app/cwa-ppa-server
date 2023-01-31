160bfa2b158a65ac55f65ebf9d315c28d62ce8d9
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2023-01-31 08:46:42 +0100

    only key_submission_metadata_with_user_metadata + key_submission_metadata_with_client_metadata

--- common/persistence/src/main/resources/db/migration/V23__alterMetadataTables.sql
+++ common/persistence/src/main/resources/db/migration/V23__alterMetadataTables.sql
@@ -1,8 +1,3 @@
-ALTER TABLE key_submission_metadata_with_user_metadata
-  ADD submission_type INTEGER;
+ALTER TABLE key_submission_metadata_with_user_metadata ADD submission_type INTEGER;
 
-ALTER TABLE key_submission_metadata_with_client_metadata
-  ADD submission_type INTEGER;
-
-ALTER TABLE exposure_risk_metadata
-  ADD submission_type INTEGER;
+ALTER TABLE key_submission_metadata_with_client_metadata ADD submission_type INTEGER;
